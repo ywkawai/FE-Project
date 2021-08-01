2fb7df26172530b9feab5bf46c97ae7cccb95b48
Yuta Kawai <yuta.kawai@riken.jp>
2021-08-01 14:22:49 +0900

    Add full discrete analysis of DG scheme applied for 1D linear advection equation.

--- sample/eigen_analysis/test_eigen_analysis.f90
+++ sample/eigen_analysis/test_eigen_analysis.f90
@@ -1,44 +1,83 @@
+!-------------------------------------------------------------------------------
+!> Program Fourier dispersion / dissipation analysis of DGM
+!!
+!! @author Team SCALE
+!!
+!! @par Reference
+!! - Alhawwary and Wang 2018:
+!!   Fourier analysis and evaluation of DG, FD and compact difference methods for conservation laws.
+!!   Journal of Computational Physics, 373, 835-862.
+!!
+!<
+!-------------------------------------------------------------------------------
 program test_eigen_analysis  
   use scale_precision
   use scale_const, only: &
     PI => CONST_PI
   use scale_io
+  use,intrinsic :: iso_fortran_env
+
+  use scale_element_line, only: LineElement
+  use scale_polynominal, only: &
+    Polynominal_GenLagrangePoly
+  use scale_polynominal, only: &
+    polynominal_genLegendrePoly,  &
+    polynominal_genDLegendrePoly, &
+    Polynominal_GenLagrangePoly
+  use scale_linalgebra, only: &
+    linalgebra_SolveLinEq
   implicit none
 
   integer, parameter :: pmax = 7
   integer :: p
 
-  real(RP), parameter :: ADV_VEL      = 1.0_RP
-  real(RP), parameter :: BETA_UPWIND  = 1.0_RP
-  real(RP), parameter :: BETA_CENTRAL = 0.0_RP
-  real(RP), parameter :: Helem        = 1.0_RP
+  real(RP), parameter :: ADV_VEL        = 1.0_RP
+  real(RP), parameter :: BETA_UPWIND    = 1.0_RP
+  real(RP), parameter :: BETA_CENTRAL   = 0.0_RP
+  real(RP), parameter :: Helem          = 1.0_RP
+  real(RP), parameter :: COURANT_NUMBER_fac = 0.5_RP
 
   integer, parameter :: nout = 10
+  complex(RP), parameter :: ei = (0.0_RP, 1.0_RP)
   !----------------------------------
 
+  !* Semi-discrete analysis
   do p=1, pmax
     call eigen_analysis( p, BETA_UPWIND, 'modal', 'weak' )
     call eigen_analysis( p, BETA_UPWIND, 'nodal', 'weak' )
   end do
 
+  !* full-discrete analysis
+
+  ! p=2, RK3
+  !  Note: To obtain the plane-line (for n=100 and k>pi/2) exactly same with that shown in Fig.9(b) of Alhawwary and Wang (2018), 
+  !        CFL need to be sligthly adjusted such that 0.5 * 0.205 although they indicated that 0.5 * 0.209.
+  call eigen_analysis( 2, BETA_UPWIND, 'modal', 'weak', 'RK3', COURANT_NUMBER_fac * 0.205_RP )
+  ! p=2, RK4
+  call eigen_analysis( 2, BETA_UPWIND, 'modal', 'weak', 'RK4', COURANT_NUMBER_fac * 0.235_RP )
+  ! p=5, RK5
+  call eigen_analysis( 5, BETA_UPWIND, 'modal', 'weak', 'RK4', COURANT_NUMBER_fac * 0.073_RP )
+
 contains
 
-  subroutine eigen_analysis( porder, beta, basis_type, form_type )
-    use scale_element_line, only: LineElement
-    use scale_polynominal, only: &
-      polynominal_genLegendrePoly, &
-      polynominal_genDLegendrePoly
+  subroutine eigen_analysis( &
+      porder, beta, basis_type, form_type, &
+      tscheme_name, courant_num )
+
     implicit none
     integer, intent(in) :: porder
     real(RP), intent(in) :: beta
     character(len=*), intent(in) :: basis_type
     character(len=*), intent(in) :: form_type
+    character(len=*), intent(in), optional :: tscheme_name
+    real(RP), intent(in), optional :: courant_num
 
     type(LineElement) :: elem
     real(RP) :: KC(porder+1,porder+1)
     real(RP) :: KM(porder+1,porder+1)
     real(RP) :: KP(porder+1,porder+1)
     real(RP) :: Stiff(porder+1,porder+1)
+    real(RP) :: Mass(porder+1,porder+1)
     real(RP) :: Minv(porder+1,porder+1)
     real(RP) :: phiM1(porder+1)
     real(RP) :: phiP1(porder+1)
@@ -48,30 +87,60 @@ contains
     integer :: p1, p2
 
     integer :: k
-    integer, parameter :: kmax = 400
+    integer, parameter :: kmax = 2000
     real(RP) :: dwnum
     real(RP) :: wnum
-    complex(RP) :: Mat(porder+1,porder+1)
+    complex(RP) :: Mat(porder+1,porder+1)    
     real(RP) :: cs_kh, si_kh
-    complex(RP), parameter :: ei = (0.0_RP, 1.0_RP)
 
     complex(RP) :: dummy(1,1)
-    complex(RP) :: vr(porder+1,porder+1), w(porder+1), rwork(2*(porder+1))
-    real(RP) :: w_r(porder+1), w_i(porder+1), dw_r(porder+1), dw_i(porder+1)
+    complex(RP) :: vr(porder+1,porder+1)
+    complex(RP) :: w(porder+1)
+    complex(RP) :: vl(porder+1,porder+1)
+    real(DP) :: rwork(2*(porder+1))
+    real(DP) :: rconde(porder+1), rcondv(porder+1), scale(porder+1)
     complex(RP), allocatable :: work(:)
+    real(RP) :: abnrm
     integer :: lwork
-    integer :: info
+    integer :: ihi, ilo, info    
+    integer :: ipiv(porder+1)
+
+    real(RP) :: w_r(porder+1), w_i(porder+1), dw_r(porder+1), dw_i(porder+1)
+    real(RP) :: w0_r(porder+1), w0_i(porder+1)
+    integer :: ipiv_eigenval(porder+1)
+
+    complex(RP) :: mu_hat(porder+1)
+    complex(RP) :: theta(porder+1)
+    complex(RP) :: EvecMat(porder+1,porder+1)
+
+
+    integer, parameter :: tlev_num = 3
+    integer, parameter :: tlev_slot(tlev_num) = (/ 1, 10, 100 /)
 
     real(RP) :: wnum_nondim(kmax)
     real(RP) :: wnum_nondim_num(2,kmax,porder+1)
+    real(RP) :: eigensol_weight(kmax,porder+1)    
+    real(RP) :: AmplifiFac_comb(tlev_num,kmax)
+    real(RP) :: PhaseError_comb(tlev_num,kmax)
     character(len=H_MID) :: filename
+
+    complex(RP) :: Ue    (porder+1,tlev_num)
+    complex(RP) :: Ue_num(porder+1,tlev_num)
+    complex(RP) :: tmpvec1(porder+1)
+    complex(RP) :: tmpvec2(porder+1)
+    real(RP) :: gex, gnum
+    real(RP) :: fac
+    real(RP) :: phase(porder+1)
+    integer :: n
+    complex(RP) :: phase_error_tmp
     !------------------------------------
 
     write(*,'(a,i2,a,a)') "Eigen analysis: porder=", porder, ', basis_type: ', trim(basis_type)
-
+    if ( present(tscheme_name) ) write(*,'(a,f12.6)') " full discrete: tscheme:" // trim(tscheme_name) // ', courant_num=', courant_num
     call elem%Init( porder, .false. )
 
     if (basis_type == 'nodal' ) then
+      Mass(:,:) = elem%M(:,:)
       Minv(:,:) = elem%invM(:,:)
       Stiff(:,:) = matmul(elem%M, elem%Sx1(:,:))
       phiM1(:) = 0.0_RP; phiP1(:) = 0.0_RP
@@ -84,15 +153,16 @@ contains
         P1D_ (:,p1) =  P1D_(:,p1) * sqrt(dble(p1-1) + 0.5_RP)
         DP1D_(:,p1) = DP1D_(:,p1) * sqrt(dble(p1-1) + 0.5_RP)
       end do
-      Minv(:,:) = 0.0_RP
+      Mass(:,:) = 0.0_RP
       do p2=1, porder+1
-        Minv(p2,p2) = 1.0_RP
+        Mass(p2,p2) = 1.0_RP
         phiM1(p2) = P1D_(1,p2)        
         phiP1(p2) = P1D_(porder+1,p2)
         do p1=1, porder+1
           Stiff(p1,p2) = sum( elem%IntWeight_lgl(:) * P1D_(:,p2) * DP1D_(:,p1) )
         end do
       end do
+      Minv(:,:) = Mass(:,:)
     else
       write(*,*) 'Check! Invalid basis_type:', basis_type
       stop
@@ -106,61 +176,178 @@ contains
 
     ! Use routine workspace query to get optimal workspace.    
     lwork = -1
-    call zgeev( 'No left vectors', 'Vectors (right)', porder+1, Mat, porder+1, w, dummy, 1, &
-      vr, porder+1, dummy, lwork, rwork, info )
-    lwork = max( (64+1)*(porder+1), nint(real(dummy(1,1))) )
+    call zgeevx( 'Balance', 'Vectors (left)', 'Vectors (right)', 'Both reciprocal condition numbers', &
+      porder+1, Mat, porder+1, w, vl, porder+1, &
+      vr, porder+1, ilo, ihi, scale, abnrm, rconde, rcondv, dummy, lwork, rwork, info )
+
+    lwork = max( (128+1)*(porder+1), nint(real(vl(1,1))) )
     allocate( work(lwork) )
   
-    dwnum = PI * dble(porder + 1) / Helem / dble( kmax )
+    dwnum = PI * dble(porder + 1) / Helem / dble( kmax-1 )
     dw_r(:) = 0.0_RP; dw_i(:) = 0.0_RP
     do k=0, kmax-1
       wnum = dble(k) * dwnum
       cs_kh = cos(wnum * Helem)
       si_kh = sin(wnum * Helem)
+
+      call calc_muhat( mu_hat(:),    &
+        wnum, elem, basis_type, Minv )
+      if ( present(tscheme_name) ) then
+        do n=1, tlev_num
+          do p1=1, porder+1
+            phase(1) = mod( dble(tlev_slot(n)) * courant_num * wnum * Helem, 2.0_RP * PI )
+            Ue(p1,n) = mu_hat(p1) * ( cos(phase(1)) - ei * sin(phase(1)) ) ! * exp(ei * k * 0.0_RP) .. xe=0
+          end do
+        end do
+      end if
+
       do p2=1, porder+1
       do p1=1, porder+1
-        Mat(p1,p2) = 2.0_RP * ( ( ( KM(p1,p2) + KP(p1,p2) ) * cs_kh + KC(p1,p2) ) * ei - ( - KM(p1,p2) + KP(p1,p2) ) * si_kh )
+        Mat(p1,p2) = 2.0_RP * ( ( ( KM(p1,p2) + KP(p1,p2) ) * cs_kh + KC(p1,p2) ) + ( - KM(p1,p2) + KP(p1,p2) ) * si_kh * ei )
       end do
       end do
+      if ( present(tscheme_name) ) then
+        call construct_full_discrete_mat( Mat, & ! (inout)
+          tscheme_name, courant_num, porder   )  ! (in)
+      end if
 
-      call zgeev( 'No left vectors', 'Vectors (right)', porder+1, Mat, porder+1, w, dummy, 1, &
-      vr, porder+1, work, lwork, rwork, info )
-    
+      call zgeevx( 'Balance', 'Vectors (left)', 'Vectors (right)', 'Both reciprocal condition numbers', &
+        porder+1, Mat, porder+1, w, vl, porder+1, vr, porder+1, &
+        ilo, ihi, scale, abnrm, rconde, rcondv, work, lwork, rwork, info )
+
       if ( info /= 0 ) then
-        write(*,*) 'Failure in ZGEEV.  INFO = ', info
+        write(*,*) 'Failure in ZGEEVX.  INFO = ', info
         stop
       end if
+
+      theta(:) = mu_hat(:)
+      EvecMat(:,:) = vr(:,:)
+      call zgesv( porder+1, 1, vr, porder+1, ipiv, theta, porder+1, info )
+            
+      if ( present(tscheme_name) ) then
+        fac =  dble(porder + 1) * courant_num
+        w_r(:) = - aimag(log(w(:))) / fac
+        w_i(:) =    real(log(w(:))) / fac
+
+        do n=1, tlev_num
+          fac = dble(porder + 1) * courant_num * dble(tlev_slot(n))
+          phase(:) = mod( fac * w_r(:), 2.0_RP * PI )
+          do p1=1, porder+1
+            Ue_num(p1,n) = sum(  EvecMat(p1,:) * theta(:) * exp( fac * w_i(:) )  &
+              * ( cos( phase(:) ) - ei * sin( phase(:) ) )                       ) ! * exp(ei * k * 0.0_RP) .. xe=0
+          end do
+        end do   
+      else
+        w_r(:) = - aimag(w(:)) / dble(porder + 1)
+        w_i(:) =    real(w(:)) / dble(porder + 1)
+      end if
       
-      w_r(:) =  real(w(:)) / dble(porder + 1)
-      w_i(:) = aimag(w(:)) / dble(porder + 1)
+      w0_r(:) = w_r(:)
+      w0_i(:) = w_i(:)
       if ( k > 0 ) then
-        call sort_eigenval( w_r, w_i, dw_r, dw_i,                  &
-          wnum_nondim_num(1,k,:), wnum_nondim_num(2,k,:), porder+1 )
+        call sort_eigenval( w_r, w_i, dw_r, dw_i, ipiv_eigenval,   & ! (out)
+          wnum_nondim_num(1,k,:), wnum_nondim_num(2,k,:), porder+1 ) ! (in)
+      else
+        forall(n=1:porder+1) ipiv_eigenval(n) = n
       end if
       wnum_nondim(k+1) = wnum * Helem / dble(porder + 1) 
       wnum_nondim_num(1,k+1,:) = w_r(:)
       wnum_nondim_num(2,k+1,:) = w_i(:)
+      eigensol_weight(k+1,:) = abs(theta(ipiv_eigenval(:)))
 
-      ! write(*,*) 'K = ', wnum_nondim(k+1)
-      ! write(*,*) 'Eigenvalues: Re', wnum_nondim_num(1,k+1,:)
-      ! write(*,*) 'Eigenvalues: Im', wnum_nondim_num(2,k+1,:)
+      do n=1, tlev_num        
+        tmpvec1(:) = matmul(Mass, Ue    (:,n))
+        tmpvec2(:) = matmul(Mass, Ue_num(:,n))
+        gex  = sqrt( real(sum( conjg(Ue    (:,n)) * tmpvec1(:) )) )
+        gnum = sqrt( real(sum( conjg(Ue_num(:,n)) * tmpvec2(:) )) )
+        phase_error_tmp = sum( conjg(Ue_num(:,n)) * tmpvec1(:) )
+
+        AmplifiFac_comb(n,k+1)  = gnum / gex
+        PhaseError_comb(n,k+1) = aimag( log( phase_error_tmp ) ) / dble( porder+1 )
+      end do
     end do
     
-    !--
+    !-- output the results for each mode
+
     do p1=1, porder+1
-      write(filename,'(a,a,a,a,a,i2.2,a,i2.2,a)') './data/eigenval_', trim(basis_type), '_', trim(form_type), '_P', porder, '_Mode', p1, '.dat'
+      if ( present( tscheme_name ) ) then 
+        write(filename,'(a,a,a,a,a,i2.2,a,i2.2,a)') './data/eigenval_', trim(basis_type), '_', trim(form_type), '_P', porder, '_Mode', p1, '_'//trim(tscheme_name)//'.dat'
+      else
+        write(filename,'(a,a,a,a,a,i2.2,a,i2.2,a)') './data/eigenval_', trim(basis_type), '_', trim(form_type), '_P', porder, '_Mode', p1, '.dat'
+      end if
       open( nout, file=trim(filename), status='replace' )
-      write( nout, '(a)') "# K Re(Km) Im(Km)"
+      write( nout, '(a)') "# K Re(Km) Im(Km) Weight"
       do k=1, kmax
-        write( nout, '(3f15.8)') wnum_nondim(k), wnum_nondim_num(1,k,p1), wnum_nondim_num(2,k,p1)
+        write( nout, '(4f20.14)') wnum_nondim(k), wnum_nondim_num(1,k,p1), wnum_nondim_num(2,k,p1), eigensol_weight(k,p1)
       end do
       close( nout )
     end do
 
+    !-- output the results for combined mode
+    if ( present( tscheme_name ) ) then 
+      write(filename,'(a,a,a,a,a,i2.2,a)') './data/eigenval_', trim(basis_type), '_', trim(form_type), '_P', porder, '_combinedmode_'//trim(tscheme_name)//'.dat'
+      open( nout, file=trim(filename), status='replace' )
+      write( nout, '(a)') "# K G(n=1) phi(n=1) G(n=10) phi(n=10)  G(n=100) phi(n=100)"
+      do k=1, kmax
+        write( nout, '(9f20.14)') wnum_nondim(k), &
+          AmplifiFac_comb(1,k), PhaseError_comb(1,k), AmplifiFac_comb(2,k), PhaseError_comb(2,k), &
+          AmplifiFac_comb(3,k), PhaseError_comb(3,k)
+      end do
+      close( nout )
+    end if
+    !-
     call elem%Final()  
 
     return
   end subroutine eigen_analysis
+
+  !----------------------------
+
+  subroutine construct_full_discrete_mat( G, & ! (inout)
+    tscheme_name, courant_num, porder )
+    implicit none
+
+    integer, intent(in) :: porder
+    complex(RP), intent(inout) :: G(porder+1,porder+1)
+    character(len=*), intent(in) :: tscheme_name
+    real(RP), intent(in) :: courant_num
+
+    complex(RP) :: A(porder+1,porder+1)
+    complex(RP) :: Id(porder+1,porder+1)
+    complex(RP) :: tmpMat(porder+1,porder+1)
+
+    integer :: rk_nstage
+    integer :: rk_s
+    integer :: m
+    !---------------------------------
+
+    select case( tscheme_name )
+    case ( 'RK2' )
+      rk_nstage = 2
+    case ( 'RK3' )
+      rk_nstage = 3
+    case ( 'RK4' )
+      rk_nstage = 4
+    case default
+      write(*,*) 'Check! Invalid tscheme_name:', tscheme_name
+      stop
+    end select
+
+    Id(:,:) = 0.0_RP
+    do m=1, porder+1
+      Id(m,m) = 1.0_RP
+    end do
+
+    A(:,:) = G(:,:)
+    G(:,:) = Id(:,:)
+    tmpMat(:,:) = Id(:,:)
+    do rk_s=1, rk_nstage
+      tmpMat(:,:) = courant_num / dble(rk_s) * matmul(tmpMat, A)
+      G(:,:) = G(:,:) + tmpMat(:,:)
+    end do
+
+    return
+  end subroutine construct_full_discrete_mat
 
   subroutine cosntruct_mat( K, Kp, Km,        &
       Stiff, Minv, phiM1, phiP1, porder, beta )
@@ -201,7 +388,49 @@ contains
     return
   end subroutine cosntruct_mat
 
-  subroutine sort_eigenval( w_r, w_i, dw_r, dw_i, &
+  !----------------------------
+
+  subroutine calc_muhat( muhat, wnum, elem, basis_type, Minv )
+    use scale_polynominal, only: Polynominal_GenLegendrePoly
+    implicit none
+    type(LineElement), intent(in) :: elem
+    complex(RP), intent(out) :: muhat(elem%Np)
+    real(RP), intent(in) :: wnum
+    character(len=*), intent(in) :: basis_type
+    real(RP), intent(in) :: Minv(elem%Np,elem%Np)
+
+    complex(RP) :: tmp(elem%Np)
+    integer, parameter :: Porder_int = 12
+    real(RP) :: basis(Porder_int+1,elem%Np)
+    type(LineElement) :: elem_int
+
+    integer :: pp
+    real(RP) :: phase(Porder_int+1)
+    !--------------------------
+
+    call elem_int%Init(Porder_int, .false.)
+
+    if (basis_type == 'nodal' ) then
+      basis(:,:) = Polynominal_GenLagrangePoly(elem%PolyOrder, elem%x1(:), elem_int%x1(:))
+    else if (basis_type == 'modal' ) then
+      basis(:,:) = Polynominal_GenLegendrePoly(elem%PolyOrder, elem_int%x1(:))
+      ! normalization
+      do pp=1, elem%PolyOrder+1
+        basis(:,pp) = basis(:,pp) * sqrt(dble(pp-1) + 0.5_RP)
+      end do      
+    end if 
+
+    do pp=1, elem%PolyOrder+1
+      phase(:) = 0.5_RP * wnum * Helem * elem_int%x1(:)
+      tmp(pp) = sum( elem_int%IntWeight_lgl(:) * ( cos(phase) + ei * sin(phase) ) * basis(:,pp) )
+    end do
+    muhat(:) = matmul( Minv, tmp )
+
+    call elem_int%Final()
+    return
+  end subroutine calc_muhat
+
+  subroutine sort_eigenval( w_r, w_i, dw_r, dw_i, ipiv, &
       wr_prev, wi_prev, n )
     use scale_quicksort, only: QUICKSORT_exec_with_idx
     implicit none
@@ -210,6 +439,7 @@ contains
     real(RP), intent(inout) :: w_i(n)
     real(RP), intent(inout) :: dw_r(n)
     real(RP), intent(inout) :: dw_i(n)
+    integer, intent(out) :: ipiv(n)
     real(RP), intent(in) :: wr_prev(n)
     real(RP), intent(in) :: wi_prev(n)
 
@@ -228,6 +458,7 @@ contains
     dw_ori(1,:) = dw_r(:)
     dw_ori(2,:) = dw_i(:)
     flag(:) = .false.
+    ipiv(:) = -9999
 
     do p1=1, n
       forall(nn=1:n) sorted_ind(nn) = nn
@@ -242,6 +473,7 @@ contains
           w_i(p1) = wi_ori(loc_min)
           dw_r(p1) = w_r(p1) - wr_prev(p1)
           dw_i(p1) = w_i(p1) - wi_prev(p1)
+          ipiv(p1) = loc_min
           exit
         end if
       end do
--- sample/eigen_analysis/visualize/dispersion_dissipation_graph.py
+++ sample/eigen_analysis/visualize/dispersion_dissipation_graph.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 DATA_DIR = "data/"
 OUT_DIR  = "visualize/"
 
+# Stability limit based on Table 3 of Alhawwary and Wang (2018). 
+CFL_max = {
+  "RK3": [0.409, 0.209, 0.13, 0.089, 0.066],
+  "RK4": [0.464, 0.235, 0.145, 0.1, 0.073]
+}
+CourantNumberFac = 0.5
+
 def set_xax(ax):
   ax.set_xlim(0.0, np.pi)
   ax.set_xticks([0, 0.25*np.pi, 0.5*np.pi, 0.75*np.pi, np.pi])
   ax.set_xticklabels(["0", "$\pi/4$", "$\pi/2$", "$3\pi/4$", "$\pi$"])
+  ax.grid(color='k', linestyle='dotted', linewidth=1)
 
 def create_fig_disp_diff(k_nondim, km_real_list, km_aimg_list, fname):
   fig = plt.figure(figsize=(10,4))
@@ -54,6 +62,76 @@ def create_fig_disp_diff_porder_dep(porder_list, k_nondim, km_real_list, km_aimg
   plt.tight_layout()  
   plt.savefig(fname)
 
+def create_fig_sgscompari_porder_dep(porder_list, k_nondim, km_real_list, km_aimg_list, fname):
+  fig = plt.figure(figsize=(10,4))
+
+  ax1 = fig.add_subplot(1,2, 1, xlabel='K', ylabel='Re(Km)')
+  ax1.set_title("dispersion")
+  set_xax(ax1)
+  ax1.plot(k_nondim, k_nondim, 'k--', label="exact")
+  for m in porder_list:
+    ax1.plot(k_nondim, km_real_list[m], label=f"p={m}")
+
+  ax2 = fig.add_subplot(1,2, 2, xlabel='K', ylabel='he/(a*(p+1)*Im(Km))')
+  ax2.set_title("decay time scale [s]")
+  set_xax(ax2)
+  ax2.set_yscale("log")
+  ax2.set_xlim(np.pi/16.0, np.pi)
+  ax2.set_ylim(5e-1, 1e5)
+
+  he_ov_pplus1 = 200/8.0#10.0
+  a = 5.0
+  Cs = 0.13
+  Dsgs=2.0*he_ov_pplus1
+  sabs = 0.15 * (np.pi/Dsgs)**(2.0/3.0)
+  for m in porder_list:
+    #print(-he_ov_pplus1/(a * (1e-15 + km_aimg_list[m])))
+    ax2.plot(k_nondim, -he_ov_pplus1/(a * (1e-15 + km_aimg_list[m])), label=f"p={m}")
+  ax2.plot(k_nondim, 1.0/( (k_nondim/he_ov_pplus1)**2 * (Cs*Dsgs)**2*sabs ), label=f"SGS time scale")
+  ax2.plot(k_nondim, 10.0/( (k_nondim/he_ov_pplus1)**2 * (Cs*Dsgs)**2*sabs ), label=f"10xSGS time scale")
+  ax2.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+
+  plt.subplots_adjust(wspace=0.4)
+  plt.tight_layout()  
+  plt.savefig(fname)
+
+def create_fig_amplification_phase_error(k_nondim,  
+  km_real, km_aimg, amplifiFac_list, phaseError_list, 
+  porder, tscheme_name, fname):
+  fig = plt.figure(figsize=(10,4))
+
+  tlev_list = [1, 10, 100]
+  color_list = ["black", "blue", "cyan"]
+
+  courant_num = CFL_max[tscheme_name][porder-1] * CourantNumberFac 
+  ax1 = fig.add_subplot(1,2, 1, xlabel='K', ylabel='Re(Km)')
+  ax1.set_yscale("log")
+  ax1.set_title("Dispersion error")
+  ax1.set_ylim(1e-10, 1e2)
+  set_xax(ax1)
+  for i in range(0,len(tlev_list)):
+    n = tlev_list[i]; color = color_list[i]
+    ax1.plot(k_nondim, n*courant_num*(np.abs(km_real-k_nondim)), color=color, linestyle=":", label=f"n={n} (physical mode)")
+    ax1.plot(k_nondim, np.abs(phaseError_list[:,i]), color=color, linestyle="-", label=f"n={n} (combined mode)")    
+
+  ax2 = fig.add_subplot(1,2, 2, xlabel='K', ylabel='G')
+  #ax2.plot(k_nondim, np.ones(len(k_nondim)), 'k', label="exact")
+  ax2.set_title("Amplification factor")
+  ax2.set_ylim(1e-12, 1e0)  
+  ax2.set_yscale("log")  
+  set_xax(ax2)
+  for i in range(0,len(tlev_list)):
+    n = tlev_list[i]; color = color_list[i]
+    ax2.plot(k_nondim, np.exp(n*(porder+1.0)*courant_num*km_aimg), color=color, linestyle=":", label=f"n={n} (physical mode)")
+    ax2.plot(k_nondim, amplifiFac_list[:,i], color=color,  linestyle="-", label=f"n={n} (combined mode)")    
+
+  ax2.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+
+  plt.subplots_adjust(wspace=0.4)
+  plt.tight_layout()  
+  plt.savefig(fname)
+
+
 # Create figure for each order of polynomial
 for basis_type in ['modal', 'nodal']:
   form_type='weak'
@@ -67,7 +145,7 @@ for basis_type in ['modal', 'nodal']:
       km_real_list[m-1] = np.loadtxt(f"{file_prefix}P{porder:02}_Mode{m:02}.dat", skiprows=1, usecols=1)
       km_aimg_list[m-1] = np.loadtxt(f"{file_prefix}P{porder:02}_Mode{m:02}.dat", skiprows=1, usecols=2)
 
-    create_fig_disp_diff(k_nondim, km_real_list, km_aimg_list, f"{OUT_DIR}/P{porder:02}_{basis_type}_{form_type}.png")
+    # create_fig_disp_diff(k_nondim, km_real_list, km_aimg_list, f"{OUT_DIR}/P{porder:02}_{basis_type}_{form_type}.png")
 
 # Create figure to show the dependence of polynomial order on the numerical dispersion/dissipation
 for basis_type in ['modal', 'nodal']:
@@ -75,7 +153,7 @@ for basis_type in ['modal', 'nodal']:
   file_prefix=f"{DATA_DIR}/eigenval_{basis_type}_{form_type}_"
   print(f"graph porderdep: {file_prefix} ..")
 
-  porder_list = [2, 4, 3, 4, 5]
+  porder_list = [2, 3, 4, 5, 6, 7]
   km_real_list_pysmode = {}; km_aimg_list_pysmode = {}
   for porder in porder_list:
     for m in range(1,porder+2):
@@ -87,4 +165,32 @@ for basis_type in ['modal', 'nodal']:
 
   k_nondim = np.loadtxt(f"{file_prefix}P{porder_list[0]:02}_Mode01.dat", skiprows=1, usecols=0)
   create_fig_disp_diff_porder_dep( porder_list, k_nondim, km_real_list_pysmode, km_aimg_list_pysmode, 
-    f"{OUT_DIR}/PolyOrderDep_{basis_type}_{form_type}.png")
+   f"{OUT_DIR}/PolyOrderDep_{basis_type}_{form_type}.png")
+
+  # create_fig_sgscompari_porder_dep( porder_list, k_nondim, km_real_list_pysmode, km_aimg_list_pysmode, 
+  #  f"{OUT_DIR}/PolyOrderDep_{basis_type}_{form_type}_sgscompari.png")
+
+# Create figure of full-discrete analysis for p=2
+for tscheme_name in ['RK3']:
+  for basis_type in ['modal']:
+    form_type='weak'
+    file_prefix=f"{DATA_DIR}/eigenval_{basis_type}_{form_type}_"
+
+    print(f"graph porderdep: {file_prefix} ..")
+
+    porder_list = [2]
+
+    for porder in porder_list:
+      k_nondim = np.loadtxt(f"{file_prefix}P{porder:02}_Mode01_{tscheme_name}.dat", skiprows=1, usecols=0)
+      amplifiFac_list = np.loadtxt(f"{file_prefix}P{porder:02}_combinedmode_{tscheme_name}.dat", skiprows=1, usecols=[1,3,5])
+      phaseError_list = np.loadtxt(f"{file_prefix}P{porder:02}_combinedmode_{tscheme_name}.dat", skiprows=1, usecols=[2,4,6])
+
+      for m in range(1,porder+2):
+        km_real = np.loadtxt(f"{file_prefix}P{porder:02}_Mode{m:02}_{tscheme_name}.dat", skiprows=1, usecols=1)
+        km_aimg = np.loadtxt(f"{file_prefix}P{porder:02}_Mode{m:02}_{tscheme_name}.dat", skiprows=1, usecols=2)
+
+        if ( abs(km_real[0]) < 1.0E-10 and km_real[1]-km_real[0] > 0.0 ):  
+          k_nondim = np.loadtxt(f"{file_prefix}P{porder_list[0]:02}_Mode01_{tscheme_name}.dat", skiprows=1, usecols=0)
+          create_fig_amplification_phase_error( 
+            k_nondim, km_real, km_aimg, amplifiFac_list, phaseError_list, 
+            porder, tscheme_name, f"{OUT_DIR}/P{porder:02}_{basis_type}_{form_type}_{tscheme_name}_G_phi.png")
