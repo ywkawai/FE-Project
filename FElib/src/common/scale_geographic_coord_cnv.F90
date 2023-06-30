c3d2989caa3e15d1f71f3e81cd9378817086eb6b
ykawai <yuta.kawai@riken.jp>
2023-06-30 15:25:02 +0900

    Interchange axes of array in arguments of subroutines

--- FElib/src/common/scale_geographic_coord_cnv.F90
+++ FElib/src/common/scale_geographic_coord_cnv.F90
@@ -39,12 +39,13 @@ module scale_geographic_coord_cnv
 
 contains
 !OCL SERIAL
-  subroutine GeographicCoordCnv_orth_to_geo_pos( geo_p, &
-    orth_p, Np )
+  subroutine GeographicCoordCnv_orth_to_geo_pos( orth_p, Np, &
+    geo_p )
+    
     implicit none
     integer, intent(in) :: Np
-    real(RP), intent(out) :: geo_p(3,Np)
-    real(RP), intent(in) :: orth_p(3,Np)
+    real(RP), intent(in) :: orth_p(Np,3)
+    real(RP), intent(out) :: geo_p(Np,3)
 
     integer :: p
     !---------------------------------------------------------  
@@ -52,17 +53,22 @@ contains
     !$omp parallel private(p)
     !$omp do
     do p=1, Np
-      geo_p(3,p) = sqrt( orth_p(1,p)**2 + orth_p(2,p)**2 + orth_p(3,p)**2 )
-      geo_p(2,p) = asin( orth_p(3,p) / geo_p(3,p) )
-      geo_p(1,p) = atan( orth_p(2,p) / orth_p(1,p) )
+      geo_p(p,3) = sqrt( orth_p(p,1)**2 + orth_p(p,2)**2 + orth_p(p,3)**2 )
+      geo_p(p,2) = asin( orth_p(p,3) / geo_p(p,3) )
+      geo_p(p,1) = atan( orth_p(p,2) / orth_p(p,1) )
     end do
     !$omp do
     do p=1, Np
-      if ( geo_p(1,p) < 0 .and. orth_p(1,p) < 0 ) then
-        geo_p(1,p) = geo_p(1,p) + PI
-      else if ( geo_p(1,p) > 0 .and. orth_p(1,p) < 0 ) then
-        geo_p(1,p) = geo_p(1,p) - PI
-      end if      
+      if ( geo_p(p,1) <= 0.0_RP .and. orth_p(p,1) < 0.0_RP ) then
+        geo_p(p,1) = geo_p(p,1) + PI
+      else if ( geo_p(p,1) >= 0.0_RP .and. orth_p(p,1) < 0.0_RP ) then
+        geo_p(p,1) = geo_p(p,1) - PI
+      end if
+      if ( orth_p(p,1) == 0.0_RP .and. orth_p(p,2) == 0.0_RP ) then
+        geo_p(p,1) = 0.0_RP
+      else if ( orth_p(p,1) == 0.0_RP ) then
+        geo_p(p,1) = sign(1.0_RP, orth_p(p,2)) * 0.5_RP * PI
+      end if   
     end do
     !$omp end parallel
 
@@ -70,37 +76,38 @@ contains
   end subroutine GeographicCoordCnv_orth_to_geo_pos
 
 !OCL SERIAL
-  subroutine GeographicCoordCnv_geo_to_orth_pos( orth_p, &
-    geo_p, Np )
+  subroutine GeographicCoordCnv_geo_to_orth_pos( geo_p, Np, &
+    orth_p )
+
     implicit none
     integer, intent(in) :: Np
-    real(RP), intent(out) :: orth_p(3,Np)
-    real(RP), intent(in) :: geo_p(3,Np)
+    real(RP), intent(in) :: geo_p(Np,3)
+    real(RP), intent(out) :: orth_p(Np,3)
 
     integer :: p
     !---------------------------------------------------------  
 
     !$omp parallel do private(p)
     do p=1, Np
-      orth_p(1,p) = geo_p(3,p) * cos(geo_p(2,p)) * cos(geo_p(1,p))
-      orth_p(2,p) = geo_p(3,p) * cos(geo_p(2,p)) * sin(geo_p(1,p))
-      orth_p(3,p) = geo_p(3,p) * sin(geo_p(2,p))
+      orth_p(p,1) = geo_p(p,3) * cos(geo_p(p,2)) * cos(geo_p(p,1))
+      orth_p(p,2) = geo_p(p,3) * cos(geo_p(p,2)) * sin(geo_p(p,1))
+      orth_p(p,3) = geo_p(p,3) * sin(geo_p(p,2))
     end do
 
     return
   end subroutine GeographicCoordCnv_geo_to_orth_pos
 
 !OCL SERIAL
-  subroutine GeographicCoordCnv_orth_to_geo_vec( geo_v, &
-    orth_v, geo_p, Np ) 
+  subroutine GeographicCoordCnv_orth_to_geo_vec( orth_v, geo_p, Np, &
+    geo_v ) 
     implicit none
 
     integer, intent(in) :: Np
-    real(RP), intent(out) :: geo_v(3,Np)
-    real(RP), intent(in) :: orth_v(3,Np)
-    real(RP), intent(in) :: geo_p(3,Np)
-    integer :: p
+    real(RP), intent(in) :: orth_v(Np,3)
+    real(RP), intent(in) :: geo_p(Np,3)
+    real(RP), intent(out) :: geo_v(Np,3)
 
+    integer :: p
     real(RP) :: sin_geo_p1
     real(RP) :: cos_geo_p1
     real(RP) :: sin_geo_p2
@@ -109,35 +116,35 @@ contains
 
     !$omp parallel do private(p, cos_geo_p2)
     do p=1, Np
-      sin_geo_p1 = sin(geo_p(1,p))
-      cos_geo_p1 = cos(geo_p(1,p))
-      sin_geo_p2 = sin(geo_p(2,p))
-      cos_geo_p2 = cos(geo_p(2,p))
+      sin_geo_p1 = sin(geo_p(p,1))
+      cos_geo_p1 = cos(geo_p(p,1))
+      sin_geo_p2 = sin(geo_p(p,2))
+      cos_geo_p2 = cos(geo_p(p,2))
 
-      geo_v(3,p) =   orth_v(1,p) * cos_geo_p2 * cos_geo_p1  &
-                   + orth_v(2,p) * cos_geo_p2 * sin_geo_p1 &
-                   + orth_v(3,p) * sin_geo_p2
+      geo_v(p,3) =   orth_v(p,1) * cos_geo_p2 * cos_geo_p1  &
+                   + orth_v(p,2) * cos_geo_p2 * sin_geo_p1 &
+                   + orth_v(p,3) * sin_geo_p2
   
-      geo_v(2,p) = - orth_v(1,p) * sin_geo_p2 * cos_geo_p1 &
-                   - orth_v(2,p) * sin_geo_p2 * sin_geo_p1 &
-                   + orth_v(3,p) * cos_geo_p2
+      geo_v(p,2) = - orth_v(p,1) * sin_geo_p2 * cos_geo_p1 &
+                   - orth_v(p,2) * sin_geo_p2 * sin_geo_p1 &
+                   + orth_v(p,3) * cos_geo_p2
     
-      geo_v(1,p) = - orth_v(1,p) * sin_geo_p1 &
-                   + orth_v(2,p) * cos_geo_p1
+      geo_v(p,1) = - orth_v(p,1) * sin_geo_p1 &
+                   + orth_v(p,2) * cos_geo_p1
     end do
 
     return
   end subroutine GeographicCoordCnv_orth_to_geo_vec
 
 !OCL SERIAL
-  subroutine GeographicCoordCnv_geo_to_orth_vec( orth_v, &
-    geo_v, geo_p, Np ) 
+  subroutine GeographicCoordCnv_geo_to_orth_vec( geo_v, geo_p, Np, &
+    orth_v ) 
     implicit none
 
     integer, intent(in) :: Np
-    real(RP), intent(out) :: orth_v(3,Np)
-    real(RP), intent(in) :: geo_v(3,Np)
-    real(RP), intent(in) :: geo_p(3,Np)
+    real(RP), intent(in) :: geo_v(Np,3)
+    real(RP), intent(in) :: geo_p(Np,3)
+    real(RP), intent(out) :: orth_v(Np,3)
 
     integer :: p
     real(RP) :: sin_geo_p1
@@ -148,32 +155,33 @@ contains
 
     !$omp parallel do private(p, sin_geo_p1, cos_geo_p1, sin_geo_p2, cos_geo_p2)
     do p=1, Np
-      sin_geo_p1 = sin(geo_p(1,p))
-      cos_geo_p1 = cos(geo_p(1,p))
-      sin_geo_p2 = sin(geo_p(2,p))
-      cos_geo_p2 = cos(geo_p(2,p))
+      sin_geo_p1 = sin(geo_p(p,1))
+      cos_geo_p1 = cos(geo_p(p,1))
+      sin_geo_p2 = sin(geo_p(p,2))
+      cos_geo_p2 = cos(geo_p(p,2))
 
-      orth_v(1,p) =   geo_v(3,p) * cos_geo_p1 * cos_geo_p2 &
-                    - geo_v(2,p) * cos_geo_p1 * sin_geo_p2 &
-                    - geo_v(1,p) * sin_geo_p1
+      orth_v(p,1) =   geo_v(p,3) * cos_geo_p1 * cos_geo_p2 &
+                    - geo_v(p,2) * cos_geo_p1 * sin_geo_p2 &
+                    - geo_v(p,1) * sin_geo_p1
 
-      orth_v(2,p) =   geo_v(3,p) * sin_geo_p1 * cos_geo_p2 &
-                    - geo_v(2,p) * sin_geo_p1 * sin_geo_p2 &
-                    + geo_v(1,p) * cos_geo_p1
+      orth_v(p,2) =   geo_v(p,3) * sin_geo_p1 * cos_geo_p2 &
+                    - geo_v(p,2) * sin_geo_p1 * sin_geo_p2 &
+                    + geo_v(p,1) * cos_geo_p1
 
-      orth_v(3,p) = geo_v(3,p) * sin_geo_p2 + geo_v(2,p) * cos_geo_p2                  
+      orth_v(p,3) = geo_v(p,3) * sin_geo_p2 + geo_v(p,2) * cos_geo_p2                  
     end do
 
     return
   end subroutine GeographicCoordCnv_geo_to_orth_vec
 
 !OCL SERIAL
-  subroutine GeographicCoordCnv_rotateX( rotated_pos_vec, &
-    pos_vec, angle )
+  subroutine GeographicCoordCnv_rotateX( pos_vec, angle, &
+    rotated_pos_vec )
+
     implicit none
-    real(RP), intent(out) :: rotated_pos_vec(3)
     real(RP), intent(in) :: pos_vec(3)
     real(RP), intent(in) :: angle
+    real(RP), intent(out) :: rotated_pos_vec(3)
 
     real(RP) :: sin_angle, cos_angle
     !---------------------------------------------------------  
@@ -189,12 +197,13 @@ contains
   end subroutine GeographicCoordCnv_rotateX
 
 !OCL SERIAL
-  subroutine GeographicCoordCnv_rotateY( rotated_pos_vec, &
-    pos_vec, angle )
+  subroutine GeographicCoordCnv_rotateY( pos_vec, angle, &
+    rotated_pos_vec )
+    
     implicit none
-    real(RP), intent(out) :: rotated_pos_vec(3)
     real(RP), intent(in) :: pos_vec(3)
     real(RP), intent(in) :: angle
+    real(RP), intent(out) :: rotated_pos_vec(3)
 
     real(RP) :: sin_angle, cos_angle
     !---------------------------------------------------------  
@@ -210,12 +219,13 @@ contains
   end subroutine GeographicCoordCnv_rotateY
 
 !OCL SERIAL
-  subroutine GeographicCoordCnv_rotateZ( rotated_pos_vec, &
-    pos_vec, angle )
+  subroutine GeographicCoordCnv_rotateZ( pos_vec, angle, &
+    rotated_pos_vec )
+
     implicit none
-    real(RP), intent(out) :: rotated_pos_vec(3)
     real(RP), intent(in) :: pos_vec(3)
     real(RP), intent(in) :: angle
+    real(RP), intent(out) :: rotated_pos_vec(3)
 
     real(RP) :: sin_angle, cos_angle
     !---------------------------------------------------------  
