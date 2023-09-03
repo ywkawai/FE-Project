1c7a73443c84508dbc1e034acde95e5cc2f23663
ykawai <yuta.kawai@riken.jp>
2023-09-03 20:19:14 +0900

    Add source files for PBL turbulence experiment

---
+++ model/atm_nonhydro3d/test/product_run_GM/pbl_turbulence/mkconf_sub.py
@@ -0,0 +1,423 @@
+import os
+import math
+import datetime
+
+
+SCALE_DG_BIN_PATH="../../../"
+SCALE_DG_REGRID_BIN_PATH="../../../../../../../../bin"
+
+#----------------------
+
+def mkconf_init( conf_path,
+                nprc, neh, nez, porder, fz, rplanet ): 
+    conf_init_s = f"""#--- Configuration file for a test case of PBL turbulence  -------
+&PARAM_IO
+ IO_LOG_BASENAME = 'init_LOG',
+/
+&PARAM_MKINIT
+  initname = 'pbl_turblence', 
+/
+&PARAM_RESTART
+  OUTPUT_FLAG = .true., 
+  OUT_BASENAME = 'init'
+/
+&PARAM_TIME
+  TIME_STARTDATE       = 0000, 1, 1, 0, 0, 0,
+  TIME_STARTMS         = 0.D0,
+/
+&PARAM_EXP
+  ENV_U          = 0D0,
+  ENV_THETA_SFC  = 299.0D0, 
+  ENV_THETA_LAPS = 4.0D-3, 
+  RANDOM_THETA   = 1.0D0, 
+/
+&PARAM_CONST
+  CONST_OHM = 0.0D0, 
+  CONST_RADIUS = {rplanet}D0,  
+/
+&PARAM_EXP
+  ENV_U          = 0D0,     
+  ENV_THETA_SFC  = 299.0D0, 
+  ENV_THETA_LAPS = 4.0D-3, 
+  RANDOM_THETA   = 1.0D0, 
+/
+&PARAM_CONST
+  CONST_OHM = 0.0D0,
+/
+#** ATMOS ******************************************************
+&PARAM_ATMOS
+  ATMOS_MESH_TYPE = 'GLOBAL', 
+  ACTIVATE_FLAG = .true., 
+  ATMOS_DYN_DO  = .true.
+/
+&PARAM_ATMOS_MESH
+  NLocalMeshPerPrc = 1, 
+  Nprc             = {nprc}, 
+  NeGX             = {neh},
+  NeGY             = {neh},
+  NeZ              = {nez},
+  dom_zmin         = 0.0D0, 
+  dom_zmax         = 3.0D3, 
+  PolyOrder_h      = {porder},
+  PolyOrder_v      = {porder}, 
+  Fz               = {fz}, 
+/
+#** ATMOS / DYN ******************************************************
+&PARAM_ATMOS_DYN
+  EQS_TYPE         = "NONHYDRO3D_HEVE", 
+  TINTEG_TYPE      = 'ERK_SSP_3s3o',    
+/    
+    """
+    
+    with open(conf_path, 'w') as f:
+        f.write(conf_init_s)
+
+#----------------
+
+def mkconf_run( conf_path, 
+                restart_in_basename, start_hour, start_min, integ_hour, 
+                nprc, neh, nez, porder, 
+                dt, dt_dyn, 
+                mf_alph, mf_ordh, mf_alpv, mf_ordv, 
+                fz, rplanet, hist_int_sec ): 
+  
+
+  conf_run_s = f"""#--- Configuration file for a test case of sound wave  -------
+&PARAM_RESTART
+  IN_BASENAME  = "{restart_in_basename}",
+  OUTPUT_FLAG  = .true., 
+  OUT_BASENAME = 'restart'
+/
+&PARAM_TIME
+  TIME_STARTDATE       = 0000, 1, 1, {start_hour}, {start_min}, 0,
+  TIME_STARTMS         = 0.D0,
+  TIME_DURATION        = {integ_hour}D0, 
+  TIME_DURATION_UNIT   = 'HOUR', 
+  TIME_DT              = {dt}D0, 
+  TIME_DT_UNIT         = 'SEC', 
+/
+&PARAM_CONST
+  CONST_OHM = 0.0D0, 
+  CONST_RADIUS = {rplanet}, 
+/
+&PARAM_USER
+  USER_do = .true., 
+/
+#** ATMOS ******************************************************
+&PARAM_ATMOS
+  ACTIVATE_FLAG       = .true., 
+  ATMOS_MESH_TYPE     = 'GLOBAL',   
+  TIME_DT             = {dt}D0, 
+  TIME_DT_UNIT        = 'SEC', 
+  ATMOS_DYN_DO        = .true., 
+  ATMOS_PHY_SF_DO = .true.,
+  ATMOS_PHY_TB_DO = .true.,   
+/
+&PARAM_ATMOS_MESH
+  NLocalMeshPerPrc = 1, 
+  Nprc             = {nprc}, 
+  NeGX             = {neh},
+  NeGY             = {neh},
+  NeZ              = {nez},
+  dom_zmin         = 0.0D0, 
+  dom_zmax         = 3.0D3, 
+  PolyOrder_h      = {porder},
+  PolyOrder_v      = {porder},
+  FZ               = {fz}, 
+  LumpedMassMatFlag = .false.,   
+/
+&PARAM_ATMOS_VARS
+  CHECK_RANGE = .true. ,
+  CHECK_TOTAL = .false.,
+/
+#** ATMOS / DYN ******************************************************
+&PARAM_ATMOS_DYN
+  EQS_TYPE         = "GLOBALNONHYDRO3D_RHOT_HEVE", 
+  !-
+  TINTEG_TYPE      = 'ERK_SSP_10s4o_2N',
+  TIME_DT          = {dt_dyn}D0, 
+  TIME_DT_UNIT     = 'SEC', 
+  !-
+  MODALFILTER_FLAG  = .true.,
+  NUMDIFF_FLAG      = .false., 
+  SPONGELAYER_FLAG  = .true.,   
+/
+&PARAM_ATMOS_DYN_BND
+  btm_vel_bc   = 'SLIP', 
+  top_vel_bc   = 'SLIP', 
+  btm_thermal_bc   = 'ADIABATIC', 
+  top_thermal_bc   = 'ADIABATIC', 
+/
+&PARAM_ATMOS_DYN_MODALFILTER
+  MF_ETAC_h  = 0.0D0, 
+  MF_ALPHA_h = {mf_alph}, 
+  MF_ORDER_h = {mf_ordh},
+  MF_ETAC_v  = 0.0D0, 
+  MF_ALPHA_v = {mf_alpv}, 
+  MF_ORDER_v = {mf_ordv},
+/
+&PARAM_ATMOS_DYN_SPONGELAYER
+  SL_WDAMP_TAU     = {dt}D0, 
+  SL_WDAMP_HEIGHT  = 2000D0, 
+/
+#** ATMOS / PHYS / SFC ******************************************************
+&PARAM_ATMOS_PHY_SFC
+  TIME_DT       = {dt}D0, 
+  TIME_DT_UNIT  = 'SEC', 
+  SFCFLX_TYPE   = 'CONST'
+/
+&PARAM_ATMOS_PHY_SF_CONST
+ ATMOS_PHY_SF_Const_SH =   200.D0,
+ ATMOS_PHY_SF_Const_LH =     0.D0,
+ ATMOS_PHY_SF_Const_Cm = 0.0011D0,
+/
+#** ATMOS / PHYS / Turbulence ***********************************************
+&PARAM_ATMOS_PHY_TB
+  TIME_DT      = {dt}D0, 
+  TIME_DT_UNIT = 'SEC', 
+  TB_TYPE      = 'SMAGORINSKY_GLOBAL'
+/
+#*** OUTPUT *******************************************
+&PARAM_FILE_HISTORY
+ FILE_HISTORY_DEFAULT_BASENAME  = "history",
+ FILE_HISTORY_DEFAULT_TINTERVAL = {hist_int_sec}D0, 
+ FILE_HISTORY_DEFAULT_TUNIT     = "SEC",
+ FILE_HISTORY_DEFAULT_TAVERAGE  = .false.,
+ FILE_HISTORY_DEFAULT_DATATYPE  = "REAL4",
+ FILE_HISTORY_OUTPUT_STEP0      = .true.,
+/
+&HISTORY_ITEM name='DDENS'        /
+&HISTORY_ITEM name='U'            /
+&HISTORY_ITEM name='V'            /
+&HISTORY_ITEM name='W'            /
+&HISTORY_ITEM name='PT'           /
+
+#*** Statistics *******************************************
+
+&PARAM_MESHFIELD_STATISTICS
+ use_globalcomm = .true.,
+/
+&PARAM_MONITOR 
+  MONITOR_STEP_INTERVAL = 50,
+/
+&MONITOR_ITEM name='DDENS' /
+&MONITOR_ITEM name='ENGT'  /
+&MONITOR_ITEM name='ENGK'  /
+&MONITOR_ITEM name='ENGI'  /
+&MONITOR_ITEM name='ENGP'  /
+    """
+    
+  with open(conf_path, 'w') as f:
+      f.write(conf_run_s)
+
+def mkconf_regrid( conf_path,
+                nprc, neh, nez, porder, fz, 
+                regrid_nprcx, regrid_nprcy, 
+                regrid_nex, regrid_ney, regrid_nez, 
+                regrid_porder, rplanet, 
+                out_dir, uniform_grid_flag ): 
+    conf_run_s = f"""#--- Configuration file for a test case of sound wave  -------
+&PARAM_IO
+ IO_LOG_BASENAME = "regrid_LOG"
+! IO_LOG_ALLNODE  = .true., 
+/
+&PARAM_CONST
+  CONST_OHM = 0.0D0, 
+  CONST_RADIUS = {rplanet}, ! R_earth / 166.7
+/
+&PARAM_REGRID_MESH
+ in_MeshType  = "CUBEDSPHERE3D", 
+ out_MeshType = "LONLAT3D", 
+/
+&PARAM_REGRID_INTERP_FIELD
+  !- input --------------------
+  in_basename="history",      
+  vars = "W", "U", "V", "DDENS", "PT", 
+  !out_tinterval = 5,
+/
+&PARAM_REGRID_FILE
+  !-- output ----------------
+  out_basename=f"./{out_dir}/history", 
+  out_UniformGrid={uniform_grid_flag}, 
+/
+&PARAM_REGRID_OPERATE_FIELD
+  uvmet_conversion_flag = .true., 
+/
+&PARAM_REGRID_INMESH3D_CUBEDSPHERE
+  NLocalMeshPerPrc = 1, 
+  Nprc             = {nprc}, 
+  NeGX             = {neh},
+  NeGY             = {neh},
+  NeGZ             = {nez},
+  dom_zmin         = 0.0D0, 
+  dom_zmax         = 3.0D3, 
+  FZ               = {fz}, 
+  PolyOrder_h      = {porder},
+  PolyOrder_v      = {porder},
+/
+&PARAM_REGRID_OUTMESH3D_STRUCTURED
+  NprcX       = {regrid_nprcx},       
+  NeX         = {regrid_nex},           
+  NprcY       = {regrid_nprcy}, 
+  NeY         = {regrid_ney},    
+  NeGZ        = {regrid_nez}, 
+  PolyOrder_h = {regrid_porder}, 
+  PolyOrder_v = {porder}, 
+  dom_xmin    =   0.0D0, 
+  dom_xmax    = 360.0D0,   
+  dom_ymin    = -90.0D0, 
+  dom_ymax    =  90.0D0, 
+  dom_zmin    = 0.0D0, 
+  dom_zmax    = 3.0D3,   
+/
+    """
+    
+    with open(conf_path, 'w') as f:
+        f.write(conf_run_s)
+
+#----------------
+def get_job_header(job_name, nprc, elapse_time):
+  node_num = math.ceil(nprc/4)
+  if node_num > 384:
+    rscgrp = "large"
+  if node_num == 384:
+    rscgrp = "large"
+    node_num=385 
+  else:
+    rscgrp = "small"
+  
+  jobshell_s = f"""################################################################################
+#
+# for Fugaku
+#
+################################################################################
+#PJM --rsc-list "rscunit=rscunit_ft01"
+#PJM --name "{job_name}"
+#PJM -x PJM_LLIO_GFSCACHE=/vol0005
+#PJM --rsc-list "rscgrp={rscgrp}"
+#PJM --rsc-list "node={node_num}"
+#PJM --rsc-list "elapse={elapse_time}"
+#PJM --mpi "max-proc-per-node=4"
+#PJM -S
+
+
+module purge
+module load lang/tcsds-1.2.37
+
+export SPACK_LIB_PATH=/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/parallel-netcdf-1.12.3-avpnzm4pwv2tuu2mv73lacb4vhcwlnds/lib:/opt/FJSVxtclanga/tcsds-mpi-latest/lib64:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/netcdf-fortran-4.6.0-mmdtg5243y4mwqsl3gcu3m2kh27raq5n/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/netcdf-c-4.9.0-g462kcd2ivou7ewax6wddywoyrbz2oib/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/hdf5-1.12.2-kb4msz2kuwzsmqsshhpryqebui6tqcfs/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/rhash-1.4.2-s3mitrsnpm36uemub4vkzj22qa4ygndu/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/libuv-1.44.1-riv7xhqvpur57jexesqfpw2mpnjjfhdd/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/libarchive-3.5.2-l7jdc7uw35jngg7tibqzsohz44ouwsj7/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/zstd-1.5.2-7j2edrlmibpft52s3m3q7ujechw3hujt/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/zlib-1.2.13-go4ye2sg72pcca4bgunmcseuzq6czbol/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/mbedtls-2.28.0-squ3v2xuqnd3mfpxiuoimtxaookk3dyi/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/lzo-2.10-uhskbd2ewdp4akltdmetra3oy4twv57f/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/libiconv-1.16-bfdxvmujixuefjz26ldcsxhzqr3rcufm/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/expat-2.4.8-lztkevt2hobbf7ykiwnuegynnoxqqvwe/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/libbsd-0.11.5-x462pikjmy4scmsuhucngco5efautbg2/lib:/vol0004/apps/oss/spack-v0.19/opt/spack/linux-rhel8-a64fx/fj-4.8.1/libmd-1.0.4-wcmufmjxfiwxa65p4eetl2y674q2pgqa/lib
+export LD_LIBRARY_PATH=/lib64:/usr/lib64:/opt/FJSVxtclanga/tcsds-latest/lib:/opt/FJSVxtclanga/tcsds-mpi-latest/lib64:${{SPACK_LIB_PATH}}:${{LD_LIBRARY_PATH}}
+
+#export XOS_MMM_L_ARENA_FREE=1
+export FORT90L="-Wl,-T"
+#export OMPI_MCA_plm_ple_memory_allocation_policy=bind_local
+export PLE_MPI_STD_EMPTYFILE="off"
+export PARALLEL=12
+export OMP_NUM_THREADS=12
+#export fu11bf=1
+
+SCALE_DG_INIT_BIN={SCALE_DG_BIN_PATH}/scale-dg_init
+SCALE_DG_BIN={SCALE_DG_BIN_PATH}/scale-dg
+SCALE_DG_REGRID_BIN={SCALE_DG_REGRID_BIN_PATH}/regrid_tool
+  """
+  return jobshell_s  
+
+def mksh_job_run( conf_path, job_name, 
+              nprc, elapse_time ):
+
+  jobshell_header_s = get_job_header(job_name, nprc, elapse_time)
+  jobshell_s = f"""
+llio_transfer ${{SCALE_DG_INIT_BIN}} ${{SCALE_DG_BIN}} *.conf
+
+mpiexec -np {nprc} -stdout-proc ./output.%j/%/1000r/stdout -stderr-proc ./output.%j/%/1000r/stderr \\
+  ${{SCALE_DG_INIT_BIN}} init.conf || exit 1
+
+mpiexec -np {nprc} -stdout-proc ./output.%j/%/1000r/stdout -stderr-proc ./output.%j/%/1000r/stderr \\
+  ${{SCALE_DG_BIN}} run.conf || exit 1  
+
+llio_transfer --purge ${{SCALE_DG_INIT_BIN}} ${{SCALE_DG_BIN}} *.conf    
+  """
+  
+  with open(conf_path, 'w') as f:
+      f.write(jobshell_header_s + jobshell_s)
+
+#----------------
+def mksh_job_regrid( conf_path, job_name, regrid_cnf, 
+              nprc, elapse_time, outdir ):
+
+  jobshell_header_s = get_job_header(job_name, nprc, elapse_time)
+  jobshell_s = f"""
+mkdir -p {outdir}/
+llio_transfer ${{SCALE_DG_REGRID_BIN}} *.conf
+
+mpiexec -np {nprc} -stdout-proc ./output.%j/%/1000r/stdout -stderr-proc ./output.%j/%/1000r/stderr \\
+  ${{SCALE_DG_REGRID_BIN}} {regrid_cnf} || exit 1
+
+llio_transfer --purge ${{SCALE_DG_REGRID_BIN}} *.conf 
+  """
+  
+  with open(conf_path, 'w') as f:
+      f.write(jobshell_header_s + jobshell_s)
+             
+def mk_conf_sh( exp_name, exp_info ):
+    nprc = exp_info["nprc"]
+    eh = exp_info["Eh"]
+    ez = exp_info["Ez"]
+    fz = exp_info["fz"]    
+    porder = exp_info["porder"]
+    rplanet = exp_info["rplanet"]
+    run_num = exp_info["run_num"]
+
+    out_dir_pref0=f"./rp{int(rplanet/1000)}km/{exp_name}"
+
+    print(out_dir_pref0)
+    os.makedirs(out_dir_pref0, exist_ok=True)
+        
+    date_time0 = datetime.datetime(1, 1, 1, 0, 0, 0, 0)
+    hr_per_run = exp_info["hour_per_run"]
+    
+    for runno in range(1,run_num+1):
+      date_time = date_time0 + datetime.timedelta(hours=hr_per_run*(runno-1))
+      
+      out_dir_pref = f"{out_dir_pref0}/run{runno}/"
+      os.makedirs(out_dir_pref, exist_ok=True)
+      
+      if runno ==1:
+        restart_in_basename = "init_00000101-000000.000"
+        mkconf_init(f"{out_dir_pref0}/run1/init.conf", 
+                    nprc, eh, ez, porder, fz, rplanet )        
+      else:
+        restart_in_basename = f"../run{runno-1}/restart_{date_time.year:04}{date_time.month:02}{date_time.day:02}-{date_time.hour:02}{date_time.minute:02}00.000", 
+
+        
+      mkconf_run(f"{out_dir_pref}/run.conf", 
+                restart_in_basename, date_time.hour, date_time.minute, hr_per_run, 
+                nprc, eh, ez, porder, 
+                exp_info["dt"], exp_info["dt_dyn"],
+                exp_info["mf_alph"], exp_info["mf_ordh"], exp_info["mf_alpv"], exp_info["mf_ordv"], 
+                fz, rplanet, exp_info["hist_int_sec"] )        
+                      
+      mkconf_regrid(f"{out_dir_pref}/regrid.conf", 
+                      nprc, eh, ez, porder, fz, 
+                      exp_info["regrid_nprcx"], exp_info["regrid_nprcy"], 
+                      exp_info["regrid_Ex"], exp_info["regrid_Ey"], exp_info["Ez"], exp_info["regrid_porder"],
+                      rplanet, "./output/", ".false." )
+
+      mkconf_regrid(f"{out_dir_pref}/regrid_uniform.conf", 
+                      nprc, eh, ez, porder, fz, 
+                      exp_info["regrid_nprcx"], exp_info["regrid_nprcy"], 
+                      exp_info["regrid_Ex"], exp_info["regrid_Ey"], exp_info["Ez"], exp_info["regrid_porder"],
+                      rplanet, "./output_uniform/", ".true." )
+      
+                  
+      mksh_job_run(f"{out_dir_pref}/job_run.sh", f"PBL_Eh{eh}P{porder}", 
+                        nprc, exp_info["elapse_time"]) 
+          
+      mksh_job_regrid(f"{out_dir_pref}/job_regrid.sh", f"REG_E{eh}P{porder}", "regrid.conf", 
+                        exp_info["regrid_nprcx"]*exp_info["regrid_nprcy"], exp_info["regrid_elapse_time"], 
+                        "outdata")
+              
+      mksh_job_regrid(f"{out_dir_pref}/job_regrid_uniform.sh", f"REG_E{eh}P{porder}", "regrid_uniform.conf", 
+                        exp_info["regrid_nprcx"]*exp_info["regrid_nprcy"], exp_info["regrid_elapse_time"], 
+                        "outdata")
+  
+#---------------------------------
---
+++ model/atm_nonhydro3d/test/product_run_GM/pbl_turbulence/mod_user.F90
@@ -0,0 +1,263 @@
+!-------------------------------------------------------------------------------
+!> module USER
+!!
+!! @par Description
+!!          User defined module for a test case of planetary boudary layer turbulence
+!!
+!! @author Team SCALE
+!!
+!<
+!-------------------------------------------------------------------------------
+#include "scalelib.h"
+module mod_user
+
+  !-----------------------------------------------------------------------------
+  !
+  !++ used modules
+  !
+  use scale_precision
+  use scale_io
+  use scale_prof
+  use scale_prc, only: PRC_abort  
+
+  use scale_const, only: &
+    PI => CONST_PI,        &
+    GRAV => CONST_GRAV,    &
+    Rdry => CONST_Rdry,    &
+    CPdry => CONST_CPdry,  &
+    CVdry => CONST_CVdry,  &
+    PRES00 => CONST_PRE00, &
+    Pstd   => CONST_Pstd  
+  
+  use mod_atmos_component, only: &
+    AtmosComponent
+
+  use scale_element_base, only: &
+    ElementBase3D, ElementBase2D
+  use scale_element_hexahedral, only: HexahedralElement
+  use scale_localmesh_3d, only: LocalMesh3D
+  use scale_localmesh_2d, only: LocalMesh2D  
+
+  use scale_sparsemat, only: &
+    SparseMat, SparseMat_matmul
+  use scale_gmres, only: &
+    GMRES
+  
+  use mod_user_base, only: UserBase
+  use mod_experiment, only: Experiment
+
+  !-----------------------------------------------------------------------------
+  implicit none
+  private
+  !-----------------------------------------------------------------------------
+  !
+  !++ Public type & procedure
+  !
+  type, public, extends(UserBase) :: User
+  contains
+    procedure :: mkinit_ => USER_mkinit
+    generic :: mkinit => mkinit_
+    procedure :: setup_ => USER_setup
+    generic :: setup => setup_
+  end type User
+
+  !-----------------------------------------------------------------------------
+  !
+  !++ Public parameters & variables
+  !
+  !-----------------------------------------------------------------------------
+  !
+  !++ Private procedure
+  !
+  !-----------------------------------------------------------------------------
+  !
+  !++ Private parameters & variables
+  !
+
+  !-----------------------------------------------------------------------------
+contains
+!OCL SERIAL
+  subroutine USER_mkinit ( this, atm )
+    implicit none
+    class(User), intent(inout) :: this
+    class(AtmosComponent), intent(inout) :: atm
+
+    type(Experiment) :: exp_manager
+    !------------------------------------------
+
+    call exp_manager%Init('idealized_pbl_turbulence')
+    call exp_manager%Regist_SetInitCond( exp_SetInitCond_pbl_turblence )
+    call this%UserBase%mkinit( atm, exp_manager )
+    call exp_manager%Final()
+
+    return
+  end subroutine USER_mkinit
+
+!OCL SERIAL
+  subroutine USER_setup( this, atm )
+    implicit none
+    class(User), intent(inout) :: this    
+    class(AtmosComponent), intent(inout) :: atm
+
+    logical :: USER_do                   = .false. !< do user step?
+    namelist / PARAM_USER / &
+       USER_do
+
+    integer :: ierr    
+    !------------------------------------------
+
+
+    LOG_NEWLINE
+    LOG_INFO("USER_setup",*) 'Setup'
+
+    !--- read namelist
+    rewind(IO_FID_CONF)
+    read(IO_FID_CONF,nml=PARAM_USER,iostat=ierr)
+    if( ierr < 0 ) then !--- missing
+       LOG_INFO("USER_setup",*) 'Not found namelist. Default used.'
+    elseif( ierr > 0 ) then !--- fatal error
+       LOG_ERROR("USER_setup",*) 'Not appropriate names in namelist PARAM_USER. Check!'
+       call PRC_abort
+    endif
+    LOG_NML(PARAM_USER)
+
+    call this%UserBase%Setup( atm, USER_do )
+    !-
+
+    return
+  end subroutine USER_setup
+
+  !------
+
+!OCL SERIAL
+  subroutine exp_SetInitCond_pbl_turblence( this,                          &
+    DENS_hyd, PRES_hyd, DDENS, MOMX, MOMY, MOMZ, DRHOT, tracer_field_list, &
+    x, y, z, dom_xmin, dom_xmax, dom_ymin, dom_ymax, dom_zmin, dom_zmax,   &
+    lcmesh, elem )
+    
+    use scale_random, only: &
+      RANDOM_uniform
+    use scale_atm_dyn_dgm_hydrostatic, only:  &
+      hydrostatic_calc_basicstate_constPTLAPS, &
+      hydrostaic_build_rho_XYZ 
+    
+    use mod_experiment, only: &
+      TracerLocalMeshField_ptr 
+         
+    implicit none
+
+    class(Experiment), intent(inout) :: this
+    type(LocalMesh3D), intent(in) :: lcmesh
+    class(ElementBase3D), intent(in) :: elem
+    real(RP), intent(out) :: DENS_hyd(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: PRES_hyd(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: DDENS(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: MOMX(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: MOMY(elem%Np,lcmesh%NeA)    
+    real(RP), intent(out) :: MOMZ(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: DRHOT(elem%Np,lcmesh%NeA)
+    type(TracerLocalMeshField_ptr), intent(inout) :: tracer_field_list(:)    
+    real(RP), intent(in) :: x(elem%Np,lcmesh%Ne)
+    real(RP), intent(in) :: y(elem%Np,lcmesh%Ne)
+    real(RP), intent(in) :: z(elem%Np,lcmesh%Ne)
+    real(RP), intent(in) :: dom_xmin, dom_xmax
+    real(RP), intent(in) :: dom_ymin, dom_ymax
+    real(RP), intent(in) :: dom_zmin, dom_zmax
+    
+    real(RP) :: ENV_PRES_SFC    
+    real(RP) :: ENV_U          = 5.0_RP
+    real(RP) :: ENV_V          = 0.0_RP
+    real(RP) :: ENV_THETA_SFC  = 298.0_RP 
+    real(RP) :: ENV_THETA_LAPS = 4.0E-3_RP
+    real(RP) :: RANDOM_THETA   = 1.0_RP
+    real(RP) :: RANDOM_U       = 0.0_RP
+    real(RP) :: RANDOM_V       = 0.0_RP        
+
+    namelist /PARAM_EXP/ &
+      ENV_U,            &
+      ENV_THETA_SFC,    &
+      ENV_THETA_LAPS,   &
+      ENV_PRES_SFC,     &
+      RANDOM_THETA
+
+    real(RP) :: rndm(elem%Np) 
+    real(RP) :: POT (elem%Np,lcmesh%NeZ,lcmesh%NeX,lcmesh%NeY)
+    real(RP) :: DENS(elem%Np)
+
+    integer :: ke, ke2D
+    integer :: ke_x, ke_y, ke_z
+    integer :: ierr
+
+    real(RP), allocatable :: bnd_SFC_PRES(:,:)
+    real(RP) :: sfc_rhot(elem%Nfp_v)
+
+    !-----------------------------------------------------------------------------
+
+    ENV_PRES_SFC = Pstd
+
+    rewind(IO_FID_CONF)
+    read(IO_FID_CONF,nml=PARAM_EXP,iostat=ierr)
+    if( ierr < 0 ) then !--- missing
+       LOG_INFO("PBL_TURBULENCE_setup",*) 'Not found namelist. Default used.'
+    elseif( ierr > 0 ) then !--- fatal error
+       LOG_ERROR("PBL_TURBULENCE_setup",*) 'Not appropriate names in namelist PARAM_EXP. Check!'
+       call PRC_abort
+    endif
+    LOG_NML(PARAM_EXP)
+
+    !---
+    
+    call hydrostatic_calc_basicstate_constPTLAPS( DENS_hyd, PRES_hyd,     &
+      ENV_THETA_LAPS, ENV_THETA_SFC, ENV_PRES_SFC,                        &
+      lcmesh%pos_en(:,:,1), lcmesh%pos_en(:,:,2), lcmesh%pos_en(:,:,3),   &
+      lcmesh, elem )
+    
+    !---
+
+    allocate( bnd_SFC_PRES(elem%Nfp_v,lcmesh%Ne2DA) )
+
+    !$omp parallel do private( ke, ke2D, rndm, sfc_rhot )
+    do ke_z=1, lcmesh%NeZ
+    do ke_y=1, lcmesh%NeY
+    do ke_x=1, lcmesh%NeX
+      ke2D = ke_x + (ke_y-1)*lcmesh%NeX
+      ke = ke2D + (ke_z-1)*lcmesh%NeX*lcmesh%NeY
+
+      call RANDOM_uniform( rndm )
+      POT(:,ke_z,ke_x,ke_y) = ENV_THETA_SFC + ENV_THETA_LAPS * z(:,ke)    &
+                            + ( rndm(:) * 2.0_RP - 1.0_RP ) * RANDOM_THETA
+      
+      if ( ke_z == 1 ) then
+        sfc_rhot(:) = DENS_hyd(elem%Hslice(:,1),ke2D) * POT(elem%Hslice(:,1),ke_z,ke_x,ke_y)        
+        bnd_SFC_PRES(:,ke2D) = PRES00 * ( Rdry * sfc_rhot(:) / PRES00 )**( CPdry/CVdry )
+      end if
+    end do
+    end do
+    end do
+    
+    call hydrostaic_build_rho_XYZ( DDENS, &
+      DENS_hyd, PRES_hyd, POT,                                            &
+      lcmesh%pos_en(:,:,1), lcmesh%pos_en(:,:,2), lcmesh%pos_en(:,:,3),   &
+      lcmesh, elem, bnd_SFC_PRES=bnd_SFC_PRES                             )
+
+    !$parallel do private( ke, DENS, rndm, ke_x, ke_y )
+    do ke_z=1, lcmesh%NeZ
+    do ke_y=1, lcmesh%NeY
+    do ke_x=1, lcmesh%NeX  
+      ke = ke_x + (ke_y-1)*lcmesh%NeX + (ke_z-1)*lcmesh%NeX*lcmesh%NeY
+
+      DENS(:) = DENS_hyd(:,ke) + DDENS(:,ke)
+      DRHOT(:,ke) = DENS(:) * POT(:,ke_z,ke_x,ke_y)                             &
+                  - DENS_hyd(:,ke) * ( ENV_THETA_SFC + ENV_THETA_LAPS * z(:,ke) )
+
+      call RANDOM_uniform( rndm )
+      MOMX(:,ke) = DENS(:) * (ENV_U + (rndm(:) * 2.0_RP - 1.0_RP ) * RANDOM_U )
+      MOMY(:,ke) = DENS(:) * (ENV_V + (rndm(:) * 2.0_RP - 1.0_RP ) * RANDOM_V )    
+    end do
+    end do
+    end do
+
+    return
+  end subroutine exp_SetInitCond_pbl_turblence
+
+end module mod_user
