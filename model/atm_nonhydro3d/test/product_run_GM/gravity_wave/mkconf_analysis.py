18b72b9dab499eba0c4b43a516fcc7a0dcdabac1
ykawai <yuta.kawai@riken.jp>
2023-09-21 10:23:55 +0900

    Modify script files to generate configuration files

--- model/atm_nonhydro3d/test/product_run_GM/gravity_wave/mkconf_analysis.py
+++ model/atm_nonhydro3d/test/product_run_GM/gravity_wave/mkconf_analysis.py
@@ -45,6 +45,34 @@ exp_list_p7 = {
                    "regrid_elapse_time": "00:60:00",
                  },             
 }
+exp_list_p7_dtx0p5 = {
+    "Eh4Ez3P7_dtx0.5": {"nprc": 6, "Eh": 4, "Ez":3, "porder": 7, 
+                   "regrid_elapse_time": "00:30:00",
+                 }, 
+    "Eh8Ez6P7_dtx0.5": {"nprc": 24, "Eh": 8, "Ez":6, "porder": 7, 
+                   "regrid_elapse_time": "00:30:00",
+                 },     
+    "Eh16Ez12P7_dtx0.5": {"nprc": 96, "Eh": 16, "Ez":12, "porder": 7, 
+                   "regrid_elapse_time": "00:60:00",
+                 },         
+    "Eh32Ez24P7_dtx0.5": {"nprc": 384, "Eh": 32, "Ez":24, "porder": 7, 
+                   "regrid_elapse_time": "00:60:00",
+                 },             
+}
+exp_list_p7_dtx0p25 = {
+    "Eh4Ez3P7_dtx0.25": {"nprc": 6, "Eh": 4, "Ez":3, "porder": 7, 
+                   "regrid_elapse_time": "00:30:00",
+                 }, 
+    "Eh8Ez6P7_dtx0.25": {"nprc": 24, "Eh": 8, "Ez":6, "porder": 7, 
+                   "regrid_elapse_time": "00:30:00",
+                 },     
+    "Eh16Ez12P7_dtx0.25": {"nprc": 96, "Eh": 16, "Ez":12, "porder": 7, 
+                   "regrid_elapse_time": "00:60:00",
+                 },         
+    "Eh32Ez24P7_dtx0.25": {"nprc": 384, "Eh": 32, "Ez":24, "porder": 7, 
+                   "regrid_elapse_time": "00:60:00",
+                 },             
+}
 # p = 11
 exp_list_p11 = {
     "Eh3Ez2P11": {"nprc": 6, "Eh": 3, "Ez":2, "porder": 11, 
@@ -57,12 +85,38 @@ exp_list_p11 = {
                    "regrid_elapse_time": "00:30:00",
                  },         
 }
+exp_list_p11_dtx0p5 = {
+    "Eh3Ez2P11_dtx0.5": {"nprc": 6, "Eh": 3, "Ez":2, "porder": 11, 
+                   "regrid_elapse_time": "00:30:00",
+                 }, 
+    "Eh6Ez4P11_dtx0.5": {"nprc": 24, "Eh": 6, "Ez":4, "porder": 11, 
+                   "regrid_elapse_time": "00:30:00",
+                 },     
+    "Eh12Ez8P11_dtx0.5": {"nprc": 96, "Eh": 12, "Ez":8, "porder": 11, 
+                   "regrid_elapse_time": "00:30:00",
+                 },         
+}
+exp_list_p11_dtx0p25 = {
+    "Eh3Ez2P11_dtx0.25": {"nprc": 6, "Eh": 3, "Ez":2, "porder": 11, 
+                   "regrid_elapse_time": "00:30:00",
+                 }, 
+    "Eh6Ez4P11_dtx0.25": {"nprc": 24, "Eh": 6, "Ez":4, "porder": 11, 
+                   "regrid_elapse_time": "00:30:00",
+                 },     
+    "Eh12Ez8P11_dtx0.25": {"nprc": 96, "Eh": 12, "Ez":8, "porder": 11, 
+                   "regrid_elapse_time": "00:30:00",
+                 },         
+}
 
 exp_list = {  
             **exp_list_p1,                          
             **exp_list_p3,              
             **exp_list_p7,  
+            **exp_list_p7_dtx0p5,                         
+            **exp_list_p7_dtx0p25,             
             **exp_list_p11,              
+            **exp_list_p11_dtx0p5, 
+            **exp_list_p11_dtx0p25,            
             }
 
 #---------------------------------
