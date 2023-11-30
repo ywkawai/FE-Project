3732eb12142b0694cb2569166ca60956dce56459
ykawai <yuta.kawai@riken.jp>
2023-11-30 16:52:13 +0900

    Modify configuration files

--- model/atm_nonhydro3d/test/product_run_GM/common/batch_job_common.py
+++ model/atm_nonhydro3d/test/product_run_GM/common/batch_job_common.py
@@ -27,7 +27,7 @@ def get_job_header( job_name, nprc, elapse_time
   node_num = math.ceil(nprc/4)
   if node_num > 384:
     rscgrp = "large"
-  if node_num == 384:
+  elif node_num == 384:
     rscgrp = "large"
     node_num = 385    
   else:
