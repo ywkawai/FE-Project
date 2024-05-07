8fe8f497b001cbfc344021ff9a7888da98007f82
ykawai <yuta.kawai@riken.jp>
2024-05-07 15:06:15 +0900

    Add files for mountain wave test

---
+++ model/atm_nonhydro3d/test/product_run_GM/mountain_wave/rhot_heve/batch_analysis.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../common'))
+import batch_job_common
+
+dir_list = [
+#* p=3
+    "Eh24Ez12P3", "Eh48Ez20P3", "Eh96Ez36P3", 
+#* p=7
+    "Eh12Ez6P7", "Eh24Ez12P7", "Eh48Ez20P7",
+#* p=11
+    "Eh8Ez5P11",  "Eh16Ez8P11", "Eh32Ez14P11",
+]
+
+def run_job(exp_dir):
+    old_path = os.getcwd()    
+    os.chdir(exp_dir)
+    print(os.getcwd())
+        
+    job_id = -1; subjob_ind = -1        
+    job_id, subjob_ind = batch_job_common.pjsub(job_id, subjob_ind, "job_regrid_analysis.sh")
+    job_id, subjob_ind = batch_job_common.pjsub(job_id, subjob_ind, "job_analysis.sh")
+
+    os.chdir(old_path)
+
+for dir in dir_list:
+    run_job(dir)
