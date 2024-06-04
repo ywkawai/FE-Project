9032fb005ab917ca00eb4772e01cdcab692f37b0
Masahiro-Ren <renxzb@gmail.com>
2024-06-04 17:51:40 +0900

    p11 tune4

--- FElib/src/fluid_dyn_solver/scale_atm_dyn_dgm_modalfilter.F90
+++ FElib/src/fluid_dyn_solver/scale_atm_dyn_dgm_modalfilter.F90
@@ -184,7 +184,7 @@ contains
     real(RP), intent(inout) :: q_in(12, 12, 12)
     real(RP), intent(inout) :: q_tmp(12, 12, 12)
     
-    real(RP) :: tmp1, tmp2
+    real(RP) :: tmp1, tmp2, tmp3
     integer :: i, j, k
 
     !-- x direction
@@ -195,18 +195,19 @@ contains
       tmp1 = filterMat(i,1)  * q_in(1,j,k) + &
              filterMat(i,2)  * q_in(2,j,k) + &
              filterMat(i,3)  * q_in(3,j,k) + & 
-             filterMat(i,4)  * q_in(4,j,k) + & 
-             filterMat(i,5)  * q_in(5,j,k) + & 
+             filterMat(i,4)  * q_in(4,j,k)
+              
+      tmp2 = filterMat(i,5)  * q_in(5,j,k) + & 
              filterMat(i,6)  * q_in(6,j,k) + & 
              filterMat(i,7)  * q_in(7,j,k) + & 
              filterMat(i,8)  * q_in(8,j,k) 
 
-      tmp2 = filterMat(i,9)  * q_in(9,j,k) + & 
+      tmp3 = filterMat(i,9)  * q_in(9,j,k) + & 
              filterMat(i,10) * q_in(10,j,k) + & 
              filterMat(i,11) * q_in(11,j,k) + & 
              filterMat(i,12) * q_in(12,j,k)  
 
-      q_tmp(i,j,k) = tmp1 + tmp2
+      q_tmp(i,j,k) = tmp1 + tmp2 + tmp3
 
     end do
     end do
@@ -220,18 +221,19 @@ contains
       tmp1 = q_tmp(i,1,k)  * filterMat_tr(1,j) + &
              q_tmp(i,2,k)  * filterMat_tr(2,j) + &
              q_tmp(i,3,k)  * filterMat_tr(3,j) + &
-             q_tmp(i,4,k)  * filterMat_tr(4,j) + &
-             q_tmp(i,5,k)  * filterMat_tr(5,j) + &
+             q_tmp(i,4,k)  * filterMat_tr(4,j)
+
+      tmp2 = q_tmp(i,5,k)  * filterMat_tr(5,j) + &
              q_tmp(i,6,k)  * filterMat_tr(6,j) + &
              q_tmp(i,7,k)  * filterMat_tr(7,j) + &
              q_tmp(i,8,k)  * filterMat_tr(8,j)
 
-      tmp2 = q_tmp(i,9,k)  * filterMat_tr(9,j) + &
+      tmp3 = q_tmp(i,9,k)  * filterMat_tr(9,j) + &
              q_tmp(i,10,k) * filterMat_tr(10,j) + &
              q_tmp(i,11,k) * filterMat_tr(11,j) + &
              q_tmp(i,12,k) * filterMat_tr(12,j) 
 
-      q_in(i,j,k) = tmp1 + tmp2
+      q_in(i,j,k) = tmp1 + tmp2 + tmp3
 
     end do
     end do
@@ -245,18 +247,19 @@ contains
       tmp1 = q_in(i,j,1)  * filterMat_tr(1,k) + &
              q_in(i,j,2)  * filterMat_tr(2,k) + &
              q_in(i,j,3)  * filterMat_tr(3,k) + &
-             q_in(i,j,4)  * filterMat_tr(4,k) + &
-             q_in(i,j,5)  * filterMat_tr(5,k) + &
+             q_in(i,j,4)  * filterMat_tr(4,k)
+
+      tmp2 = q_in(i,j,5)  * filterMat_tr(5,k) + &
              q_in(i,j,6)  * filterMat_tr(6,k) + &
              q_in(i,j,7)  * filterMat_tr(7,k) + &
              q_in(i,j,8)  * filterMat_tr(8,k)
 
-      tmp2 = q_in(i,j,9)  * filterMat_tr(9,k) + &
+      tmp3 = q_in(i,j,9)  * filterMat_tr(9,k) + &
              q_in(i,j,10) * filterMat_tr(10,k) + &
              q_in(i,j,11) * filterMat_tr(11,k) + &
              q_in(i,j,12) * filterMat_tr(12,k) 
 
-      q_tmp(i,j,k) = tmp1 + tmp2
+      q_tmp(i,j,k) = tmp1 + tmp2 + tmp3
 
     end do
     end do
