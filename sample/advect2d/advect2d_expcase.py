a8b582a00c2f6dc8e33cf38f9dfa23922406bc95
Yuta Kawai <yuta.kawai@riken.jp>
2025-01-17 11:15:37 +0900

    Update scripts to generate config files for 2d advection test

--- sample/advect2d/advect2d_expcase.py
+++ sample/advect2d/advect2d_expcase.py
@@ -11,42 +11,61 @@ class Advect2DCase(ExpCase):
     self._neX = neX; self._neY = neY
     self._polyOrder = polyOrder
     self._tint_scheme = tint_scheme
+    self._do_NumErrorAnalysis = False
     super(Advect2DCase, self).__init__( exp, path_from_exptopdir, name, "test.conf",   
                                         duration_sec, dt_sec, 
                                        "test_advect2d", 1, 1)
   
-  def set_initShape(self, iniShapeName, iniShapeParams, iniGalerkinProjFlag):
+  def set_initShape(self, iniShapeName, iniShapeParams, iniGPMat_polyOrder):
     self._iniShapeName = iniShapeName
     self._iniShapeParams = iniShapeParams
-    self._iniGalerkinProjFlag = iniGalerkinProjFlag
+    self._iniGPMat_polyOrder = iniGPMat_polyOrder
 
   def set_velType(self, velTypeName, velTypeParams):
     self._velTypeName = velTypeName
     self._velTypeParams = velTypeParams
 
+  def set_eval_numerror(self, polyorder_eval_error, nstep_eval_error):
+    self._do_NumErrorAnalysis = True
+    self._polyorder_eval_error = polyorder_eval_error
+    self._nstep_eval_error = nstep_eval_error
+  
   def generate_configfile(self): 
 
     ishape_params = "D0, ".join(map(str, self._iniShapeParams)) + "D0"
     veltype_params = "D0, ".join(map(str, self._velTypeParams)) + "D0"
     conf = []
-    nstep_eval_error = int(0.1/self._dt_sec)
     conf.append( textwrap.dedent('''\
         &PARAM_TEST
           NeGX             = {neX}, 
           NeGY             = {neY}, 
           PolyOrder        = {polyOrder},
+          !** Shape of inital q ******************************
           InitShapeName    = '{iniShapeName}',
           InitShapeParams  = {ishape_params},
-          InitCond_GalerkinProjFlag = {iniGalerkinProjFlag}, 
+          InitGPMatPolyOrder = {iniGPMat_polyOrder}          
+          !** Type of advection velocity **********************
           VelTypeName    = '{velTypeName}',
-          VelTypeParams  = {veltype_params},        
-          nstep_eval_error = {nstep_eval_error}, 
-          TINTEG_SCHEME_TYPE = 'RK_TVD_3',  
+          VelTypeParams  = {veltype_params}, 
+          Do_NumErrorAnalysis = {do_NumErrorAnalysis},          
+          !----------------------------------------------------
+          TINTEG_SCHEME_TYPE = '{tint_scheme}',       
         /
     ''').format( \
       neX=self._neX, neY=self._neY, polyOrder=self._polyOrder, 
-      iniShapeName=self._iniShapeName, ishape_params=ishape_params, iniGalerkinProjFlag=self._iniGalerkinProjFlag, 
-      velTypeName=self._velTypeName, veltype_params=veltype_params, nstep_eval_error=nstep_eval_error) )
+      iniShapeName=self._iniShapeName, ishape_params=ishape_params, iniGPMat_polyOrder=self._iniGPMat_polyOrder, 
+      velTypeName=self._velTypeName, veltype_params=veltype_params, do_NumErrorAnalysis= '.true.' if self._do_NumErrorAnalysis else '.false.', 
+      tint_scheme=self._tint_scheme  ) )
+
+    if self._do_NumErrorAnalysis:
+      conf.append( textwrap.dedent('''\
+          &PARAM_ADVECT2D_NUMERROR
+            PolyOrderErrorCheck = {polyorder_eval_error},
+            LOG_OUT_BASENAME    = 'LOG_NUMERROR', 
+            LOG_STEP_INTERVAL   = {nstep_eval_error}, 
+          /
+      ''').format( \
+        polyorder_eval_error=self._polyorder_eval_error, nstep_eval_error=self._nstep_eval_error ) )
     
     conf.append( textwrap.dedent('''\
         &PARAM_TIME
@@ -73,7 +92,8 @@ class Advect2DCase(ExpCase):
       f.write( "\n".join(conf) )
 
 
-def exp_prepair(TOP_DIR, Ne_List, PolyOrder_List, DELT_List, ORG_PE_PATH, expcase_params_dict, iniGalerkinProjFlag): 
+def exp_prepair(TOP_DIR, Ne_List, PolyOrder_List, DELT_List, ORG_PE_PATH, expcase_params_dict, 
+                iniGPMat_porder_list, eval_numerror_porder_list, eval_numerror_step_list): 
   exp = Experiment(TOP_DIR, "advect2d", ORG_PE_PATH)
 
   for p_i, p in enumerate(PolyOrder_List):
@@ -86,9 +106,11 @@ def exp_prepair(TOP_DIR, Ne_List, PolyOrder_List, DELT_List, ORG_PE_PATH, expcas
         path = "I"+iniShapeName+"/V"+velTypeName+"/"+"Ne"+str(ne)+"P"+str(p)
         case_name = expcase_prefix + "_Ne"+str(ne)+"P"+str(p)
 
-        case = Advect2DCase(exp, path, case_name, ne, ne, p, 'RK_TVD_3', 5.0, DELT_List[p_i][ne_i])
-        case.set_initShape(iniShapeName, iniShapeParams, iniGalerkinProjFlag)
+        case = Advect2DCase(exp, path, case_name, ne, ne, p, 'ERK_SSP_3s3o', 5.0, DELT_List[p_i][ne_i])
+        case.set_initShape(iniShapeName, iniShapeParams, iniGPMat_porder_list[p_i])
         case.set_velType(velTypeName, velTypeParams)
+        if velTypeName=="constant" or velTypeName=="rigid-body-rot":
+          case.set_eval_numerror(eval_numerror_porder_list[p_i], eval_numerror_step_list[ne_i])
         exp.add_expcase(case)
 
   exp.prepair()
