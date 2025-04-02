ab56767a38d09ac18212b75ee712799939080851
Yuta Kawai <yuta.kawai@riken.jp>
2025-04-02 10:06:39 +0900

    Implement a solver to treat linear equations with a band matrix

--- FElib/src/common/scale_linalgebra.F90
+++ FElib/src/common/scale_linalgebra.F90
@@ -34,6 +34,7 @@ module scale_linalgebra
     module procedure linalgebra_SolveLinEq_b2D
   end interface
   public :: linalgebra_SolveLinEq
+  public :: linalgebra_SolveLinEq_BndMat
   public :: linalgebra_SolveLinEq_GMRES
 
 
@@ -56,7 +57,14 @@ module scale_linalgebra
 
   ! Private procedure
   !
+  private :: my_dger
+  private :: my_idamax
+  private :: my_swap
 
+  private :: PreCondStep_ILU0_constructmat
+  private :: PreCondStep_ILU0_solve
+  private :: PreCondStep_PtJacobi
+  
   ! Private variable
   !
 
@@ -64,7 +72,7 @@ contains
 
 !OCL SERIAL
   function linalgebra_inv(A) result(Ainv)
-
+    implicit none
     real(RP), intent(in) :: A(:,:)
     real(RP) :: Ainv(size(A,1),size(A,2))
 
@@ -94,7 +102,7 @@ contains
 
 !OCL SERIAL
   subroutine linalgebra_SolveLinEq_b1D(A, b, x)
-
+    implicit none
     real(RP), intent(in) :: A(:,:)
     real(RP), intent(in) :: b(:)
     real(RP), intent(out) :: x(size(b))
@@ -126,7 +134,7 @@ contains
 
 !OCL SERIAL
   subroutine linalgebra_SolveLinEq_b2D(A, b, x)
-
+    implicit none
     real(RP), intent(in) :: A(:,:)
     real(RP), intent(in) :: b(:,:)
     real(RP), intent(out) :: x(size(b,1),size(b,2))
@@ -158,7 +166,7 @@ contains
 
 !OCL SERIAL
   subroutine linalgebra_LU(A_lu, ipiv)
-
+    implicit none
     real(RP), intent(inout) :: A_lu(:,:)
     integer, intent(out) :: ipiv(size(A_lu,1))
 
@@ -177,8 +185,132 @@ contains
     return
   end subroutine linalgebra_LU
 
-  subroutine linalgebra_SolveLinEq_GMRES(A, b, x, m, restart_num, CONV_CRIT)
 
+!> Calculate the solution of linear equations with a band matrix 
+!!
+!! In this module, we treat a linear equations system, A * X = B
+!! where A is a band matrix of order N with KL subdiagonals and KU superdiagonals, 
+!! and X and B are matrices whose size is N * NRHS.  
+!! 
+!! @param A Matrix A in band storage same as in dgbsv of LAPACK
+!! @param b Right hand side matrix B with N-by-RHS 
+!! @param ipiv Pivot indices that define the permutation matrix
+!! @param KL Number of subdiagonals within the band of A
+!! @param KU Number of superdiagonals within the band of A
+!! @param NRHS Number of right hand sides
+!! @param use_lapack Flag whether LAPACK is used
+!OCL SERIAL
+  subroutine linalgebra_SolveLinEq_BndMat(A, b, ipiv, &
+    N, KL, KU, NRHS, use_lapack )
+    implicit none
+    integer, intent(in) :: N
+    integer, intent(in) :: KL
+    integer, intent(in) :: KU
+    integer, intent(in) :: NRHS
+    real(RP), intent(inout) :: A(2*KL+KU+1,N)
+    real(RP), intent(inout) :: b(N,NRHS)
+    integer, intent(out) :: ipiv(N)
+    logical, intent(in), optional :: use_lapack
+
+    integer :: i, j, l, lm, p
+    integer :: km
+    integer :: ju, jp
+    integer :: KV
+    real(RP) :: tmp
+    integer :: LDA, LDB
+    logical :: use_lapack_
+
+    integer :: info
+    !--------------------------------------------------------------------------- 
+
+    if (present(use_lapack)) then
+      use_lapack_ = use_lapack
+    else
+      use_lapack_ = .false.
+    end if
+
+    if ( use_lapack_ ) then
+      call dgbsv( N, KL, KU, NRHS, A, 2*kl+ku+1, ipiv, b, N, info)
+      return
+    end if
+
+    !--
+    KV = KL + KU
+    LDA = size(A,1)
+    LDB = size(B,1)
+
+    do j=KU+2, min(KV, N)
+    do i=KV-j+2, KL
+      A(i,j) = 0.0_RP
+    end do
+    end do
+    
+    ju = 1
+    do j=1, N
+      if ( j+KV <= N ) then
+        do i=1, KL
+          A(i,j+KV) = 0.0_RP
+        end do
+      end if
+
+      km = min(KL, N-j)
+      call my_idamax( km+1, A(KV+1,j), &
+        jp )
+      ipiv(j) = jp + j - 1
+
+      if ( A(KV+jp,j) /= 0.0_RP ) then
+        ju = max( ju, min(j+KU+jp-1,N) )
+
+        if ( jp /= 1 ) then
+          call my_swap( ju-j+1, A(KV+jp,j), LDA-1, &
+                                A(KV +1,j), LDA-1  )
+        end if
+
+        if ( km > 0 ) then
+          tmp = 1.0_RP / A(KV+1,j)
+          do l=KV+2, KV+1+km
+            A(l,j) = tmp * A(l,j)
+          end do
+          call my_dger( km, ju-j, &
+            A(KV+2,j  ),          &
+            A(KV  ,j+1), LDA-1,   &
+            A(KV+1,j+1), LDA-1    )
+        end if
+      end if
+    end do
+    
+    !--
+    ! Solve L*X = B, overwriting B with X
+
+    if ( kl > 0 ) then
+      do j=1, N-1
+        lm = min( KL, N-j )
+        l = ipiv(j)
+        if ( l /= j ) call my_swap( NRHS, b(l,1), LDB, b(j,1), LDB )
+        call my_dger( lm, NRHS, &
+          A(KV+2,j),     &
+          b(j  ,1), LDB, &
+          b(j+1,1), LDB  )
+      end do
+    end if
+
+    !--
+    ! Solve U*X = B, overwriting B with X
+    do p=1, NRHS
+      do j=N, 1, -1
+        l = KV + 1 - j
+        b(j,p) = b(j,p) / A(KV+1,j)
+        tmp = b(j,p)
+        do i=j-1, max(1,j-KV),-1
+          b(i,p) = b(i,p) - tmp * A(l+i,j)
+        end do
+      end do 
+    end do
+
+    return
+  end subroutine linalgebra_SolveLinEq_BndMat
+
+  subroutine linalgebra_SolveLinEq_GMRES(A, b, x, m, restart_num, CONV_CRIT)
     use scale_sparsemat, only: sparsemat_matmul
     implicit none
     
@@ -273,6 +405,107 @@ contains
     return
   end subroutine linalgebra_SolveLinEq_GMRES
 
+!- private ----------------------------------------------------------------
+
+!OCL SERIAL
+  subroutine my_idamax( N, DX, ind )
+    implicit none
+    integer, intent(in) :: N
+    real(RP), intent(in) :: DX(*)
+    integer, intent(out) :: ind
+
+    integer :: i
+    real(RP) :: dmax
+    !----------------------------
+    dmax = abs(dx(1))
+    ind = 1
+    do i=2, N
+      if ( abs(dx(i)) > dmax ) then
+        ind = i
+        dmax = abs(dx(i))
+      end if
+    end do
+    return
+  end subroutine my_idamax
+
+!> Calculate A_ij = A_ij - x_i y_j 
+!OCL SERIAL
+  subroutine my_dger( m, n, x, y, incy, A, LDA)
+    implicit none
+    integer, intent(in) :: m
+    integer, intent(in) :: n
+    integer, intent(in) :: LDA    
+    real(RP), intent(inout) :: A(LDA,*)
+    real(RP), intent(in) :: x(*)
+    real(RP), intent(in) :: y(*)
+    integer, intent(in) :: incy
+
+    integer :: i, j
+    integer :: jy
+    real(RP) :: tmp
+    !---------------------------------------------------
+    jy = 1
+    do j=1, n
+      tmp = y(jy)
+      do i=1, m
+        A(i,j) = A(i,j) - tmp * x(i)
+      end do
+      jy = jy + incy
+    end do
+    return
+  end subroutine my_dger
+
+!> Interchange two vectors
+!!
+!! Note that incx, incy should be > 0
+!OCL SERIAL
+  subroutine my_swap( n, dx, incx, dy, incy )
+    implicit none
+    integer, intent(in) :: n
+    real(RP), intent(inout) :: dx(*)
+    integer, intent(in) :: incx
+    real(RP), intent(inout) :: dy(*)
+    integer, intent(in) :: incy
+
+    integer :: i, ix, iy
+    integer :: m, mp1
+    real(RP) :: dtemp
+    !---------------------------------------------------
+    if ( incx==1 .and. incy==1 ) then
+      m = mod(n,3)
+      if (m /= 0) then
+        do i=1, m
+          dtemp = dx(i)
+          dx(i) = dy(i)
+          dy(i) = dtemp
+        end do
+        mp1 = m + 1
+        do i=mp1, n, 3
+          dtemp = dx(i)
+          dx(i) = dy(i)
+          dy(i) = dtemp
+          dtemp = dx(i+1)
+          dx(i+1) = dy(i+1)
+          dy(i+1) = dtemp
+          dtemp = dx(i+2)
+          dx(i+2) = dy(i+2)
+          dy(i+2) = dtemp
+        end do
+      end if
+    else
+      ix = 1; iy = 1
+      do i=1, n
+        dtemp = dx(ix)
+        dx(ix) = dy(iy)
+        dy(iy) = dtemp
+        ix = ix + incx
+        iy = iy + incy
+      end do
+    end if
+    return
+  end subroutine my_swap
+
+!OCL SERIAL
   subroutine PreCondStep_PtJacobi(A, b, x)
     implicit none
     type(sparsemat), intent(in) :: A
@@ -289,6 +522,7 @@ contains
     return
   end subroutine PreCondStep_PtJacobi
 
+!OCL SERIAL
   subroutine PreCondStep_ILU0_constructmat(A, M)
     use scale_const, only: &
       EPS => CONST_EPS
@@ -325,6 +559,7 @@ contains
     return
   end subroutine PreCondStep_ILU0_constructmat
 
+!OCL SERIAL
   subroutine PreCondStep_ILU0_solve(M, b, x)
     use scale_sparsemat, only: &
       SPARSEMAT_STORAGE_TYPEID_CSR
