59bfba49032576d3e4c5c993e637a6c021988573
ykawai <yuta.kawai@riken.jp>
2025-06-13 11:49:33 +0900

    Update configuration files

--- model/atm_nonhydro3d/test/case/rising_thermal_bubble/moist_mp_tomita08/visualize/visualize.sh
+++ model/atm_nonhydro3d/test/case/rising_thermal_bubble/moist_mp_tomita08/visualize/visualize.sh
@@ -5,7 +5,7 @@ mkdir -p analysis
 
 ### check error norm ###
 echo "+mkgraph monitor"
-for var in DDENS QV QC QR ENGT ENGP ENGK ENGI; do
+for var in DDENS QV QC QI QR QS QG ENGT ENGP ENGK ENGI; do
    python ../../common/cmd_analysis_monitor.py monitor.peall ${var} 3.0 analysis/monitor_${var}.png
 done
 
@@ -14,15 +14,17 @@ echo "+mkgraph"
 for time in 300 600 900 1200 1500 1800; do
    python ../../common/cmd_mkgraph.py history.pe00*.nc@U,y=16e3,time=${time} analysis/U_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4 --range -20 20 --int 2
    python ../../common/cmd_mkgraph.py history.pe00*.nc@W,y=16e3,time=${time} analysis/W_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4 --range -40 40 --int 5
-   python ../../common/cmd_mkgraph.py history.pe00*.nc@QC,y=16e3,time=${time} analysis/QC_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4
-   python ../../common/cmd_mkgraph.py history.pe00*.nc@QR,y=16e3,time=${time} analysis/QR_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4
+   for qtrc in QC QI QR QS QG; do
+      python ../../common/cmd_mkgraph.py history.pe00*.nc@${qtrc},y=16e3,time=${time} analysis/${qtrc}_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4
+   done
 done
 
 for time in 2700 3600 5400; do
    python ../../common/cmd_mkgraph.py history.pe00*.nc@U,y=16e3,time=${time} analysis/U_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4 --range -20 20 --int 2
    python ../../common/cmd_mkgraph.py history.pe00*.nc@W,y=16e3,time=${time} analysis/W_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4 --range -20 20 --int 2.5
-   python ../../common/cmd_mkgraph.py history.pe00*.nc@QC,y=16e3,time=${time} analysis/QC_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4
-   python ../../common/cmd_mkgraph.py history.pe00*.nc@QR,y=16e3,time=${time} analysis/QR_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4
+   for qtrc in QC QI QR QS QG; do
+      python ../../common/cmd_mkgraph.py history.pe00*.nc@${qtrc},y=16e3,time=${time} analysis/${qtrc}_y16km_t${time}s.png --prc_num_xy 5 2 --figsize 12 4
+   done
 done
 
 python ../../common/cmd_mkgraph.py history.pe00*.nc@PREC,y=16e3 analysis/PREC_y16km.png --exch --prc_num_xy 5 2 --figsize 12 6
