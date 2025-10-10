e94b95d10d509018ea19959d000023de1bb2eb94
Yuta Kawai <yuta.kawai@riken.jp>
2025-10-10 09:15:48 +0900

    Modify a driver such that  a large-scale condensation scheme is available

--- FElib/src/microphysics/scale_atm_phy_mp_lscond.F90
+++ FElib/src/microphysics/scale_atm_phy_mp_lscond.F90
@@ -27,8 +27,9 @@ module scale_atm_phy_mp_lscond
     PRES00 => CONST_PRE00, &
     Grav => CONST_GRAV
   
-  use scale_localmesh_3d, only: &
-    LocalMesh3D
+  use scale_element_base, only: &
+    ElementBase1D, ElementBase3D
+  use scale_localmesh_3d, only: LocalMesh3D
   use scale_mesh_base3d, only: MeshBase3D
 
   !-----------------------------------------------------------------------------
@@ -40,6 +41,8 @@ module scale_atm_phy_mp_lscond
   !
   public :: ATMOS_PHY_MP_lscond_setup
   public :: ATMOS_PHY_MP_lscond_adjustment
+  public :: ATMOS_PHY_MP_lscond_precipitation
+  public :: ATMOS_PHY_MP_lscond_precipitation_momentum
 
   !-----------------------------------------------------------------------------
   !++ Public parameters & variables
@@ -49,13 +52,13 @@ module scale_atm_phy_mp_lscond
   integer,                parameter, public :: ATMOS_PHY_MP_lscond_ntracers = QA_MP
   integer,                parameter, public :: ATMOS_PHY_MP_lscond_nwaters = 1
   integer,                parameter, public :: ATMOS_PHY_MP_lscond_nices = 0
-  character(len=H_SHORT), parameter, public :: ATMOS_PHY_MP_kessler_tracer_names(QA_MP) = (/ &
+  character(len=H_SHORT), parameter, public :: ATMOS_PHY_MP_lscond_tracer_names(QA_MP) = (/ &
        'QV', &
        'QC'  /)
-  character(len=H_MID)  , parameter, public :: ATMOS_PHY_MP_kessler_tracer_descriptions(QA_MP) = (/ &
+  character(len=H_MID)  , parameter, public :: ATMOS_PHY_MP_lscond_tracer_descriptions(QA_MP) = (/ &
        'Ratio of Water Vapor mass to total mass (Specific humidity)', &
        'Ratio of Cloud Water mass to total mass                    '  /)
-  character(len=H_SHORT), parameter, public :: ATMOS_PHY_MP_kessler_tracer_units(QA_MP) = (/ &
+  character(len=H_SHORT), parameter, public :: ATMOS_PHY_MP_lscond_tracer_units(QA_MP) = (/ &
        'kg/kg',  &
        'kg/kg'   /)
 
@@ -77,7 +80,7 @@ module scale_atm_phy_mp_lscond
   logical,  private              :: flag_liquid = .true.     ! warm rain
   logical,  private              :: couple_aerosol = .false. ! Consider CCN effect ?
 
-  real(RP), private, parameter   :: re_qc =  8.E-6_RP        ! effective radius for cloud water
+  ! real(RP), private, parameter   :: re_qc =  8.E-6_RP        ! effective radius for cloud water
 
 contains
 
@@ -164,10 +167,10 @@ contains
 
 !OCL SERIAL
   subroutine ATMOS_PHY_MP_lscond_precipitation( &
-    DENS, RHOQ, CPtot, CVtot, RHOE,         & ! (inout)
-    FLX_hydro, sflx_rain, sflx_snow, esflx, & ! (inout)
-    TEMP, dt,                               & ! (in)
-    QHA, QLA, QIA, lcmesh, elem, elem1D     ) ! (in)
+    DENS, RHOQ, CPtot, CVtot, RHOE,            & ! (inout)
+    sflx_rain, sflx_snow, esflx,               & ! (inout)
+    TEMP,                                      & ! (in)
+    QHA, QLA, QIA, lcmesh, elem, elem1D        ) ! (in)
     
     use scale_const, only: &
       UNDEF => CONST_UNDEF8
@@ -175,10 +178,7 @@ contains
       CV_WATER, &
       CP_WATER, &
       CV_ICE,   &
-      CP_ICE
-    
-    use scale_element_base, only: &
-      ElementBase1D, ElementBase3D
+      CP_ICE    
     implicit none
 
     class(LocalMesh3D), intent(in) :: lcmesh
@@ -189,12 +189,10 @@ contains
     real(RP), intent(inout) :: CPtot(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
     real(RP), intent(inout) :: CVtot(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
     real(RP), intent(inout) :: RHOE (elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
-    real(RP), intent(inout) :: FLX_hydro(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
     real(RP), intent(inout) :: sflx_rain(elem%Nfp_v,lcmesh%Ne2DA)
     real(RP), intent(inout) :: sflx_snow(elem%Nfp_v,lcmesh%Ne2DA)
     real(RP), intent(inout) :: esflx    (elem%Nfp_v,lcmesh%Ne2DA)
     real(RP), intent(in) :: TEMP (elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
-    real(RP), intent(in) :: dt
     integer, intent(in) :: QLA, QIA
     class(ElementBase1D), intent(in) :: elem1D
 
@@ -257,8 +255,6 @@ contains
           condens_vint_lc(p2D) = sum( vint_weight(:,p2D) * dDENS(elem%Colmask(:,p2D)) )
         end do
       
-        ! FLX_hydro(:,ke_z,ke2D) = FLX_hydro(:,ke_z,ke2D) &
-        !                         + qflx(:)
         if ( ke_z == 1 ) then
           if ( iq > QLA ) then ! ice water
               sflx_snow(:,ke2D) = sflx_snow(:,ke2D)  &
@@ -304,4 +300,44 @@ contains
     return
   end subroutine ATMOS_PHY_MP_lscond_precipitation
 
+!OCL SERIAL
+  subroutine ATMOS_PHY_MP_lscond_precipitation_momentum( &
+    MOMU_t, MOMV_t, MOMZ_t,                & ! (out)
+    DENS, MOMU, MOMV, MOMZ, DENS_new,      & ! (in)
+    rdt_MP, lcmesh, elem                   ) ! (in)
+    implicit none
+
+    class(LocalMesh3D), intent(in) :: lcmesh
+    class(ElementBase3D), intent(in) :: elem
+    real(RP), intent(out) :: MOMU_t(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: MOMV_t(elem%Np,lcmesh%NeA)
+    real(RP), intent(out) :: MOMZ_t(elem%Np,lcmesh%NeA)
+    real(RP), intent(in) :: DENS(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
+    real(RP), intent(in) :: MOMU(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
+    real(RP), intent(in) :: MOMV(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
+    real(RP), intent(in) :: MOMZ(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
+    real(RP), intent(in) :: DENS_new(elem%Np,lcmesh%NeZ,lcmesh%Ne2D)
+    real(RP), intent(in) :: rdt_MP
+    
+    integer :: ke2D
+    integer :: ke_z
+    integer :: ke
+    real(RP) :: coef(elem%Np)
+    !----------------------------------------------------------
+
+    !$omp parallel do collapse(2) private( &
+    !$omp ke2D, ke_z, ke, coef )
+    do ke2D = 1, lcmesh%Ne2D
+    do ke_z = 1, lcmesh%NeZ
+      ke = ke2D + (ke_z-1)*lcmesh%Ne2D
+      coef(:) = ( DENS_new(:,ke_z,ke2D) / DENS(:,ke_z,ke2D) - 1.0_RP ) * rdt_MP
+
+      MOMU_t(:,ke) = coef(:) * MOMU(:,ke_z,ke2D)
+      MOMV_t(:,ke) = coef(:) * MOMV(:,ke_z,ke2D)
+      MOMZ_t(:,ke) = coef(:) * MOMZ(:,ke_z,ke2D)
+    end do
+    end do
+    return
+  end subroutine ATMOS_PHY_MP_lscond_precipitation_momentum
+  
 end module scale_atm_phy_mp_lscond
