789fe5dea799c430fb533180a0ce63c068d5d875
ykawai <yuta.kawai@riken.jp>
2026-06-30 19:55:11 +0900

    Implement gray radiation scheme and optical depth calculations

--- FElib/src/radiation/scale_atm_phy_rd_dgm_gray.F90
+++ FElib/src/radiation/scale_atm_phy_rd_dgm_gray.F90
@@ -17,6 +17,9 @@ module scale_atm_phy_rd_dgm_gray
   use scale_prc
   use scale_prof
 
+  use scale_atmos_phy_rd_common, only: &
+    I_up, I_dn, I_LW, I_SW
+  
   use scale_element_base, only: &
     ElementBase1D, ElementBase2D, ElementBase3D
   use scale_element_hexahedral, only: HexahedralElement
@@ -26,26 +29,61 @@ module scale_atm_phy_rd_dgm_gray
   use scale_localmeshfield_base, only: LocalMeshField3D
   use scale_meshfield_base, only: MeshField3D
 
+  !-----------------------------------------------------------------------------
+  implicit none
+  private  
+  !-----------------------------------------------------------------------------
+  !
+  !++ Public type & procedure
+  !
+
   type, public :: AtmPhyRadGray
-    integer :: optdep_type
+    integer :: optdep_type                  !< Type of optical depth calculation
+    real(RP) :: OPTDEP_VALLIS_EQ5_PARAMS(4) !< Parameters associated with optical depth calculation (Eq.5 in Vallis et al. (2018))
+                                            !! A, mu, B, C
 
-    real(RP) :: OPTDEP_VALLIS_EQ5_PARAM_A
-    real(RP) :: OPTDEP_VALLIS_EQ5_PARAM_MU
-    real(RP) :: OPTDEP_VALLIS_EQ5_PARAM_C
+    real(RP) :: pCO2 !< CO2 concentration in ppmv
+    real(RP) :: diffFactor !< Diffusivity factor for the two-stream approximation
   contains
+    procedure, public :: Init  => atm_phy_rd_dgm_gray_Init
+    procedure, public :: Final => atm_phy_rd_dgm_gray_Final
+    procedure, public :: calculate_rad_flux  => atm_phy_rd_dgm_gray_flux
+    procedure, private :: calc_optical_thick
   end type AtmPhyRadGray
-
-  integer, parameter :: OPTDEP_TYPE_VALLIS2018_EQ5 = 1
   
-contains
-!OCL SERIAL
-  subroutine atm_phy_rd_dgm_gray_Init( mesh )
-    implicit none    
-    class(MeshBase3D), intent(in) :: mesh
+  !-----------------------------------------------------------------------------
+  !++ Public parameters & variables
+  !
+  !-----------------------------------------------------------------------------
+  !
+  !++ Private procedure
+  !
+  !-----------------------------------------------------------------------------
+  !
+  !++ Private parameters & variables
+  !  
+  integer, parameter :: OPTDEP_TYPE_VALLIS2018_EQ5 = 1
 
-    character(len=H_MID) :: OPTICAL_DEPTH_TYPE
+contains
+  !> Initialize an object to represent a gray-radiation scheme
+!OCL SERIAL
+  subroutine atm_phy_rd_dgm_gray_Init( this )
+    implicit none
+    class(AtmPhyRadGray), intent(inout) :: this
+
+    character(len=H_MID) :: OPTICAL_DEPTH_TYPE            = 'Vallis2018_Eq5' !< How to calculate optical depth. 'Vallis2018_Eq5'
+    character(len=H_MID) :: OPTDEP_VALLIS_EQ5_PARAMS_TYPE = 'V2018'          !< How to specify the parameters for optical depth calculation (Eq.5 in Vallis et al. (2018)). 'BO2013', 'V2018', 'USER'
+    real(RP) :: OPTDEP_VALLIS_EQ5_PARAMS(4) !< Array of parameters for optical depth calculation (Eq.5 in Vallis et al. (2018)). A, mu, B, C
+
+    real(RP) :: DiffusivityFactor = 1.66_RP !< Diffusivity factor for the two-stream approximation
+    real(RP) :: pCO2 = 360.0_RP             !< CO2 concentration in ppmv
+
     namelist / PARAM_ATMOS_PHY_RD_DGM_GRAY / &
-      OPTICAL_DEPTH_TYPE
+      OPTICAL_DEPTH_TYPE,            &
+      OPTDEP_VALLIS_EQ5_PARAMS_TYPE, &
+      OPTDEP_VALLIS_EQ5_PARAMS,      &
+      DiffusivityFactor,             &
+      pCO2
         
     integer :: ierr
     !--------------------------------------------------------------------
@@ -69,67 +107,158 @@ contains
       this%optdep_type = OPTDEP_TYPE_VALLIS2018_EQ5
     case default
       LOG_ERROR("ATMOS_PHY_RD_dgm_gray_setup",*) 'OPTICAL_DEPTH_TYPE is invalid. Check!', trim(OPTICAL_DEPTH_TYPE)
+      call PRC_abort
     end select
+
+    this%pCO2 = pCO2
+    this%diffFactor = DiffusivityFactor
+
+    if ( this%optdep_type == OPTDEP_TYPE_VALLIS2018_EQ5 ) then
+      select case(trim(OPTDEP_VALLIS_EQ5_PARAMS_TYPE))
+      case('BO2013')
+        this%OPTDEP_VALLIS_EQ5_PARAMS = [ 0.8678_RP, 1.0_RP, 1997.9_RP, 0.0_RP ]
+      case('V2018')
+        this%OPTDEP_VALLIS_EQ5_PARAMS = [ 0.1627_RP, 1.0_RP, 1997.9_RP, 0.17_RP ]
+      case('USER')
+        this%OPTDEP_VALLIS_EQ5_PARAMS(:) = OPTDEP_VALLIS_EQ5_PARAMS(:)
+      case default
+        LOG_ERROR("ATMOS_PHY_RD_dgm_gray_setup",*) 'OPTDEP_VALLIS_EQ5_PARAMS_TYPE is invalid. Check!', trim(OPTDEP_VALLIS_EQ5_PARAMS_TYPE)
+        call PRC_abort
+      end select
+    end if
     return
   end subroutine atm_phy_rd_dgm_gray_Init
 
+  !> Finalize an object to represent a gray-radiation scheme
 !OCL SERIAL
-  subroutine atm_phy_rd_dgm_gray_Final()
+  subroutine atm_phy_rd_dgm_gray_Final(this)
     implicit none
+    class(AtmPhyRadGray), intent(inout) :: this
     !--------------------------------------------------------------------
     return
   end subroutine atm_phy_rd_dgm_gray_Final
 
 !OCL SERIAL
-  subroutine atm_phy_rd_dgm_gray_flux( flux, &
-    DENS, TEMP, &
-    lcmesh, elem3D, lcmesh2D, elem2D, elem_v1D )
+  subroutine atm_phy_rd_dgm_gray_flux( this, &
+    flux, &
+    PRES, TEMP, DENS, QV, SFC_TEMP,  &
+    lcmesh, elem3D, lcmesh2D, elem2D )
+    use scale_const, only: &
+      PI => CONST_PI, &
+      STB => CONST_STB
+    implicit none
+    class(AtmPhyRadGray), intent(inout) :: this
     class(LocalMesh3D), intent(in) :: lcmesh
     class(ElementBase3D), intent(in) :: elem3D
     class(LocalMesh2D), intent(in) :: lcmesh2D
     class(ElementBase2D), intent(in) :: elem2D
-    class(ElementBase1D), intent(in) :: elem_v1D
-    implicit none
-    real(RP), intent(in) :: flux(elem3D%Nnode_v,lcmesh%NeZ,,elem3D%Nnode_h1D**2,lcmesh%Ne2D,2,2,2)
-    real(RP), intent(in) :: TEMP(elem3D%Nnode_v,lcmesh%NeZ,,elem3D%Nnode_h1D**2,lcmesh%Ne2D)
-    real(RP), intent(in) :: DENS(elem3D%Nnode_v,lcmesh%NeZ,,elem3D%Nnode_h1D**2,lcmesh%Ne2D)
+    real(RP), intent(out) :: flux(elem3D%Nnode_h1D**2,elem3D%Nnode_v,lcmesh%Ne2D,lcmesh%NeZ,2,2)
+    real(RP), intent(in) :: PRES(elem3D%Nnode_v,lcmesh%NeZ,elem3D%Nnode_h1D**2,lcmesh%Ne2D)
+    real(RP), intent(in) :: TEMP(elem3D%Nnode_v,lcmesh%NeZ,elem3D%Nnode_h1D**2,lcmesh%Ne2D)
+    real(RP), intent(in) :: DENS(elem3D%Nnode_v,lcmesh%NeZ,elem3D%Nnode_h1D**2,lcmesh%Ne2D)
+    real(RP), intent(in) :: QV(elem3D%Nnode_v,lcmesh%NeZ,elem3D%Nnode_h1D**2,lcmesh%Ne2D)
+    real(RP), intent(in) :: SFC_TEMP(elem2D%Np,lcmesh2D%NeA)
 
-    integer :: ke_z, ke_h
-    integer :: p_z, p_h
+    integer :: ke, ke_z, ke_h
+    integer :: p, p_z, p_h
 
-    real(RP) :: dtau(elem3D%Nnode_v,lcmesh%NeZ)
+    real(RP) :: dtau(elem3D%Nnode_v-1,lcmesh%NeZ)
     real(RP) :: TransFunc(elem3D%Nnode_v,lcmesh%NeZ)
+    real(RP) :: CO2(elem3D%Nnode_v,lcmesh%NeZ)
+    real(RP) :: trans(elem3D%Nnode_v-1,lcmesh%NeZ) !< Transmission across the layer
+    real(RP) :: Src(elem3D%Nnode_v-1,lcmesh%NeZ)   !< Source term across the layer
+    real(RP) :: temp_
+    real(RP) :: D
 
+    real(RP) :: flux_up(elem3D%Nnode_v,lcmesh%NeZ)
+    real(RP) :: flux_dn(elem3D%Nnode_v,lcmesh%NeZ)
     !--------------------------------------------------------------------
-    !$omp parallel do
+    !--------------------------------------------------------------------
+
+    D = this%diffFactor
+
+    !$omp parallel do private(CO2, trans, temp_, flux_up, flux_dn, Src, dtau) collapse(2)
     do ke_h=1, lcmesh%Ne2D
     do p_h=1, elem3D%Nnode_h1D**2
+      CO2(:,:) = this%pCO2
+      call this%calc_optical_thick( dtau,               &
+        PRES(:,:,p_h,ke_h), QV(:,:,p_h,ke_h), CO2(:,:), &
+        elem3D%Nnode_v, lcmesh%NeZ                      )
+
+      trans(:,:) = exp(- D * dtau(:,:))
+
+      !--
+      do ke_z=1, lcmesh%NeZ
+      do p_z=1, elem3D%Nnode_v-1
+        temp_ = 0.5_RP * ( TEMP(p_z,ke_z,p_h,ke_h) + TEMP(p_z+1,ke_z,p_h,ke_h) )
+        Src(p_z,ke_z) = temp_**4 * STB
+      end do
+      end do
+
+      flux_dn(elem3D%Nnode_v,lcmesh%NeZ) = 0.0_RP
+      do ke_z=lcmesh%NeZ, 1, -1
+        do p_z=elem3D%Nnode_v-1, 1, -1
+          flux_dn(p_z,ke_z) = flux_dn(p_z+1,ke_z) * trans(p_z,ke_z) + Src(p_z,ke_z) * ( 1.0_RP - trans(p_z,ke_z) )
+        end do
+        if ( ke_z > 1 ) then
+          flux_dn(elem3D%Nnode_v,ke_z-1) = flux_dn(1,ke_z)
+        end if
+      end do
+
+      flux_up(1,1) = SFC_TEMP(p_h,ke_h)**4 * STB
+      do ke_z=1, lcmesh%NeZ
+        do p_z=1, elem3D%Nnode_v-1
+          flux_up(p_z+1,ke_z) = flux_up(p_z,ke_z) * trans(p_z,ke_z) + Src(p_z,ke_z) * ( 1.0_RP - trans(p_z,ke_z) )
+        end do
+        if ( ke_z < lcmesh%NeZ ) then
+          flux_up(1,ke_z+1) = flux_up(elem3D%Nnode_v,ke_z)
+        end if
+      end do
+
       do ke_z=1, lcmesh%NeZ
       do p_z=1, elem3D%Nnode_v
-        dtau(p_z,ke_z) = 
+        flux(p_h,p_z,ke_h,ke_z,I_LW,I_up) = flux_up(p_z,ke_z)
+        flux(p_h,p_z,ke_h,ke_z,I_LW,I_dn) = flux_dn(p_z,ke_z)
+
+        flux(p_h,p_z,ke_h,ke_z,I_SW,I_up) = 0.0_RP
+        flux(p_h,p_z,ke_h,ke_z,I_SW,I_dn) = 0.0_RP
       end do
       end do
     end do
     end do
     return
-  end subroutine atm_phy_tb_dgm_gray_flux
+  end subroutine atm_phy_rd_dgm_gray_flux
 
 !-- private -----
-  subroutine calc_optical_thick( dtau, &
-    pres, qv, Nnode_v, NeZ )
+!OCL SERIAL
+  subroutine calc_optical_thick( this, dtau, &
+    pres, qv, CO2, Nnode_v, NeZ )
     implicit none
+    class(AtmPhyRadGray), intent(in) :: this
     integer, intent(in) :: Nnode_v
     integer, intent(in) :: NeZ
     real(RP), intent(out) :: dtau(Nnode_v-1,NeZ)
     real(RP), intent(in) :: pres(Nnode_v,NeZ)
     real(RP), intent(in) :: qv(Nnode_v,NeZ)
+    real(RP), intent(in) :: CO2(Nnode_v,NeZ)
 
     integer :: ke_z, p_z
+    real(RP) :: A, B, C
+    real(RP) :: dsig
+
+    real(RP), parameter :: P0 = 1.0e5_RP
+    real(RP) :: qv_tmp
     !---------------------------------------------------------
+
+    A = this%OPTDEP_VALLIS_EQ5_PARAMS(1) * this%OPTDEP_VALLIS_EQ5_PARAMS(2)
+    B = this%OPTDEP_VALLIS_EQ5_PARAMS(3)
+    C = this%OPTDEP_VALLIS_EQ5_PARAMS(4)
 
     do ke_z=1, NeZ
     do p_z=1, Nnode_v-1
-      dtau(p_z,ke_z) = 
+      qv_tmp = 0.5_RP * ( qv(p_z,ke_z) + qv(p_z+1,ke_z) )
+      dsig = max( pres(p_z,ke_z) - pres(p_z+1,ke_z), 0.0_RP ) / P0
+      dtau(p_z,ke_z) = ( A + B * qv_tmp + C * log( CO2(p_z,ke_z) / 360.0_RP ) ) * dsig
     end do
     end do
     return
